6f9b9497163592d560adcdb2259d43fc77c37b00
Neil Mitchell <ndmitchell@meta.com>
2025-04-13 04:59:14 -0700

    Fail if the vulnerability upgrade script fails
    Summary: Should be present on basically all shell scripts.
    
    Reviewed By: SamChou19815
    
    Differential Revision: D72917611
    
    fbshipit-source-id: a5e3ac1f1c4cf2b9749296c2a75281af45f2a038

--- website/scripts/audit_and_fix_vulnerabilities.sh
+++ website/scripts/audit_and_fix_vulnerabilities.sh
@@ -4,6 +4,9 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+set -e
+set -o pipefail
+
 # Script to run yarn audit and fix security vulnerabilities
 
 # Change to website directory
