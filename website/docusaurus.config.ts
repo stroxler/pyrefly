031e50ffa60c083f9d2f768b4f33e1d2ee603ee3
Maggie Moss <maggiemoss@meta.com>
2025-04-23 16:46:34 -0700

    Add copy command buttons to code examples
    Summary:
    I followed these instructions: https://gist.github.com/yangshun/55db997ed0f8f4e6527571fc3bee4675
    
    That then adds a button to each code sample for users to easily copy / paste
    
    Reviewed By: Solumin
    
    Differential Revision: D73532664
    
    fbshipit-source-id: c628229d6dcb49da8aecf31e5117b03645dcbee8

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -264,6 +264,12 @@ const config: Config = {
             },
         ],
     ],
+    scripts: [
+        'https://buttons.github.io/buttons.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.0/clipboard.min.js',
+        '/js/code-block-buttons.js',
+      ],
+      stylesheets: ['/css/code-block-buttons.css']
 };
 
 export default config;
