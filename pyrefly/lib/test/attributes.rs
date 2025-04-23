6f563fc7314d72173dff48a30c1a43b8f4a5333b
Steven Troxler <stroxler@meta.com>
2025-04-23 14:42:04 -0700

    Add test cases for class-object `__getattr__`
    Summary:
    When we try to access an attribute on a class object, the runtime does not
    try to use the `__getattr__` method defined on the class - it will only use
    that for *instance* lookups.
    
    Instead, it tries to use the metaclass `__getattr__`.
    
    Pyrefly's current lookup logic produces two bugs here: first, we try to use
    the `__getattr__` defined on the class, which leads to call errors since
    it is an unbound method when accessed on the class itself. Second, the
    class `__getattr__`, if defined, will *hide* the metaclass `__getattr__`,
    thereby producing a large number of type errors and potentially leading
    to bad type inference if the two don't return the same type.
    
    Note that in the case where the class *doesn't* define `__getattr__`, the
    dispatch against the metaclass is working fine, it's only when that is
    shadowed that we get bad behavior.
    
    Reviewed By: rchen152
    
    Differential Revision: D73522691
    
    fbshipit-source-id: c04d45ab69d21349ed09c8325d30ad7b5d24af61

--- pyrefly/lib/test/attributes.rs
+++ pyrefly/lib/test/attributes.rs
@@ -770,3 +770,39 @@ def f(d: D):
     assert_type(d.x, Any)
     "#,
 );
+
+testcase!(
+    test_getattr_dispatch_for_metaclass,
+    r#"
+from typing import assert_type
+class EMeta(type):
+    def __getattr__(self, attr: str) -> int: ...
+class E(int, metaclass=EMeta):
+    pass
+assert_type(E.EXAMPLE_VALUE, int)
+    "#,
+);
+
+testcase!(
+    bug = "TODO(stroxler) We are selecting the wrong `__getattr__` for class object access - it comes from the metaclass",
+    test_getattr_selection_for_class_object_w_metaclass,
+    r#"
+from typing import assert_type
+class EMeta(type):
+    def __getattr__(self, attr: str) -> int: ...
+class E(metaclass=EMeta):
+    def __getattr__(self, attr: str) -> str: ...
+assert_type(E.EXAMPLE_VALUE, int)  # E: assert_type(str, int) # E: Missing argument `attr` in function `E.__getattr__`  # E: Argument `Literal['EXAMPLE_VALUE']` is not assignable to parameter `self` with type `Self@E` in function `E.__getattr__` 
+    "#,
+);
+
+testcase!(
+    bug = "TODO(stroxler) We should be complaining about a missing attribute here, not throwing errors analyzing a call.",
+    test_getattr_selection_for_class_object_no_metaclass,
+    r#"
+from typing import assert_type
+class E:
+    def __getattr__(self, attr: str) -> str: ...
+E.EXAMPLE_VALUE # E: Missing argument `attr` in function `E.__getattr__`  # E: Argument `Literal['EXAMPLE_VALUE']` is not assignable to parameter `self` with type `Self@E` in function `E.__getattr__` 
+    "#,
+);
