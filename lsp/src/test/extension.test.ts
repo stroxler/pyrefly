9e7ec562c9c3ee80087c4f4fd9bdec97797ada24
Rebecca Chen (Python) <rechen@meta.com>
2025-05-09 20:27:16 -0700

    Increase timeout for extension test
    Summary:
    test_extension is sometimes failing: https://github.com/facebook/pyrefly/actions/runs/14940864076/job/41977764460.
    
    The failure message says that we're hitting a timeout of 2000 ms. The test seems happy after I increased the timeout to 5000 ms: https://github.com/rchen152/pyrefly/actions/runs/14940926912/job/41977931542.
    
    Reviewed By: yangdanny97
    
    Differential Revision: D74520364
    
    fbshipit-source-id: edd0613e9f52f2ea0dcbf7d343026d9e0221fe3a

--- lsp/src/test/extension.test.ts
+++ lsp/src/test/extension.test.ts
@@ -4,7 +4,8 @@ import * as vscode from 'vscode';
 suite('Extension Test Suite', () => {
 	const extension: vscode.Extension<unknown> | undefined = vscode.extensions.getExtension('meta.pyrefly');
 
-	test('Test activation', async () => {
+	test('Test activation', async function () {
+		this.timeout(5000);
 		await extension?.activate();
 		assert.ok(true);
 	});
