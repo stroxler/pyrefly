c8ce3507fe28d23b87c503f8525859fd7d887274
David Luo <dluo@meta.com>
2025-05-13 10:45:17 -0700

    replace usage of useColorMode for codesnippet
    Summary: Similar to D74598188, we shouldn't use `useColorMode` during the initial load as it could load stale values. This diff makes use of `html[data-theme='dark']` in css styles instead to distinguish between light and dark mode.
    
    Reviewed By: stroxler
    
    Differential Revision: D74627121
    
    fbshipit-source-id: 212a929eac03bc247b812a05d0ab75e3ef0a699c

--- website/src/sandbox/CodeSnippet.tsx
+++ website/src/sandbox/CodeSnippet.tsx
@@ -7,7 +7,6 @@
 
 import BrowserOnly from '@docusaurus/BrowserOnly';
 import * as React from 'react';
-import * as docusaurusTheme from '@docusaurus/theme-common';
 import * as stylex from '@stylexjs/stylex';
 
 const Sandbox = React.lazy(() => import('./Sandbox'));
@@ -22,16 +21,12 @@ export default function CodeSnippet({
     sampleFilename,
     codeSample = '',
 }: CodeSnippetProps): JSX.Element {
-    const { colorMode } = docusaurusTheme.useColorMode();
-
     if (sampleFilename == null) {
         throw "Missing sampleFilename. IDE services won't work properly.";
     }
 
     return (
-        <pre
-            {...stylex.props(colorMode === 'light' ? styles.preLightMode : {})}
-        >
+        <pre {...stylex.props(styles.pre)}>
             <BrowserOnly>
                 {() => (
                     <React.Suspense fallback={<div>Loading...</div>}>
@@ -48,7 +43,7 @@ export default function CodeSnippet({
 }
 
 const styles = stylex.create({
-    preLightMode: {
-        backgroundColor: '#e0e0e0', // Use darker grey in light mode
+    pre: {
+        backgroundColor: 'var(--code-snippet-background)',
     },
 });
