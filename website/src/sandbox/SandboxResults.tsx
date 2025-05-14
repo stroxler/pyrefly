fcc2ca58d9ba265b98ebecadaaea535ad5e0916d
David Luo <dluo@meta.com>
2025-05-13 21:46:34 -0700

    wrap line for long strings in sandbox results
    Summary: Just noticed that on the mobile sandbox, there's no line wrap when there's a long string, and a scroll bar shows up. Adding line wrapping which makes this look much nicer
    
    Reviewed By: stroxler
    
    Differential Revision: D74681669
    
    fbshipit-source-id: b74a352b603fb21fdfab64808db1dd3bb1f5d424

--- website/src/sandbox/SandboxResults.tsx
+++ website/src/sandbox/SandboxResults.tsx
@@ -234,6 +234,10 @@ const styles = stylex.create({
         paddingHorizontal: '10px',
         marginBottom: 0,
         background: 'var(--color-background)',
+        width: '100%',
+        whiteSpace: 'pre-wrap',
+        wordBreak: 'break-word',
+        display: 'block',
     },
     tabs: {
         display: 'flex',
