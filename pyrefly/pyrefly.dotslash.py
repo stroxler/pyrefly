43d4767e499c69df6a22a4841e2a7ac0cbeacbd4
Jia Chen <grievejia@meta.com>
2025-05-17 07:01:07 -0700

    Build pyrefly dotslash in more platforms
    Summary: Make it possible to run the dotslash pyrefly on more platforms
    
    Reviewed By: ndmitchell, stroxler
    
    Differential Revision: D74836188
    
    fbshipit-source-id: 5ad3ca1e2592297ee5bdc2fa01f627a3ee7ac12f

--- pyrefly/pyrefly.dotslash.py
+++ pyrefly/pyrefly.dotslash.py
@@ -10,4 +10,11 @@ import dotslash
 dotslash.export_fbcode_build(
     target="fbcode//pyrefly/pyrefly:fbpyrefly",
     oncall="pyre",
+    install_platforms={
+        dotslash.InstallPlatform.LINUX_AARCH64,
+        dotslash.InstallPlatform.LINUX_X86_64,
+        dotslash.InstallPlatform.MAC_AARCH64,
+        dotslash.InstallPlatform.MAC_X86_64,
+        dotslash.InstallPlatform.WINDOWS,
+    },
 )
