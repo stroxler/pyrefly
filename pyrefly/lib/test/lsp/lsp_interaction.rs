c1da189621cdf60a586688e4ed5d07434edbed38
Xavi Simpson <xavisimpson@meta.com>
2025-06-20 13:45:31 -0700

    Add test for empty file with synthetic `textDocument/didChange` notification
    Summary: Added test to ensure non-breaking handling empty file on filesystem while supplying file contents with `textDocument/didChange`.
    
    Reviewed By: SamChou19815
    
    Differential Revision: D76852686
    
    fbshipit-source-id: 17c5c816aff4223550a45c1005c49d7f7790eb77

--- pyrefly/lib/test/lsp/lsp_interaction.rs
+++ pyrefly/lib/test/lsp/lsp_interaction.rs
@@ -553,6 +553,97 @@ fn test_module_completion() {
 }
 
 #[test]
+fn test_empty_filepath_file_completion() {
+    let root = get_test_files_root();
+    let empty_filename = root.path().join("empty_file.py");
+
+    run_test_lsp(TestCase {
+        messages_from_language_client: vec![
+            Message::from(Notification {
+                method: "textDocument/didOpen".to_owned(),
+                params: serde_json::json!({
+                    "textDocument": {
+                        "uri": Url::from_file_path(&empty_filename).unwrap().to_string(),
+                        "languageId": "python",
+                        "version": 1,
+                        "text": String::default(),
+                    }
+                }),
+            }),
+            Message::from(Notification {
+                method: "textDocument/didChange".to_owned(),
+                params: serde_json::json!({
+                    "textDocument": {
+                        "uri": Url::from_file_path(&empty_filename).unwrap().to_string(),
+                        "languageId": "python",
+                        "version": 2
+                    },
+                    "contentChanges": [{
+                        "text": format!("{}\n{}\n", std::fs::read_to_string(root.path().join("notebook.py")).unwrap(), "t")
+                    }],
+                }),
+            }),
+            Message::from(Request {
+                id: RequestId::from(2),
+                method: "textDocument/completion".to_owned(),
+                params: serde_json::json!({
+                    "textDocument": {
+                        "uri": Url::from_file_path(&empty_filename).unwrap().to_string()
+                    },
+                    "position": {
+                        "line": 9,
+                        "character": 1
+                    }
+                }),
+            }),
+            Message::from(Notification {
+                method: "textDocument/didChange".to_owned(),
+                params: serde_json::json!({
+                    "textDocument": {
+                        "uri": Url::from_file_path(&empty_filename).unwrap().to_string(),
+                        "languageId": "python",
+                        "version": 3
+                    },
+                    "contentChanges": [{
+                        "text": format!("{}\n{}", std::fs::read_to_string(root.path().join("notebook.py")).unwrap(), "t")
+                    }],
+                }),
+            }),
+            Message::from(Request {
+                id: RequestId::from(3),
+                method: "textDocument/completion".to_owned(),
+                params: serde_json::json!({
+                    "textDocument": {
+                        "uri": Url::from_file_path(&empty_filename).unwrap().to_string()
+                    },
+                    "position": {
+                        "line": 10,
+                        "character": 1
+                    }
+                }),
+            }),
+        ],
+        expected_messages_from_language_server: vec![
+            Message::Response(Response {
+                id: RequestId::from(2),
+                result: Some(
+                    serde_json::json!({"isIncomplete":false,"items":[{"detail":"(a: int, b: int, c: str) -> int","kind":3,"label":"tear","sortText":"0"}]}),
+                ),
+                error: None,
+            }),
+            Message::Response(Response {
+                id: RequestId::from(3),
+                result: Some(
+                    serde_json::json!({"isIncomplete":false,"items":[{"detail":"(a: int, b: int, c: str) -> int","kind":3,"label":"tear","sortText":"0"}]}),
+                ),
+                error: None,
+            }),
+        ],
+        ..Default::default()
+    });
+}
+
+#[test]
 fn test_references() {
     let root = get_test_files_root();
     let root_path = root.path().join("tests_requiring_config");
---
+++ pyrefly/lib/test/lsp/test_files/notebook.py
@@ -0,0 +1,8 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+
+def tear(a: int, b: int, c: str) -> int:
+    return a + b + int(c)
