231ac8d432416208ff10faf5d67ce57f935710f8
Steven Troxler <stroxler@meta.com>
2025-06-23 03:56:54 -0700

    Move `class_tparams` to the top of `class_metadata.rs`
    Summary:
    This concept is much simpler and more primitive than `ClassMetadata`,
    so it makes more sense to have it defined above rather than buried
    in the middle of the rest of the metadata logic.
    
    It likely ought to be moved elsewhere eventually, but I'm not ready
    to do that yet; it's a little tricky since both tparams and metadata
    share a reliance on `BaseClass`, which makes moving it out bring up
    a lot of bikeshedding questions I'm not ready to deal with right
    now.
    
    Reviewed By: ndmitchell
    
    Differential Revision: D77126630
    
    fbshipit-source-id: cada0cb22807616c1230a1f9add2ca4af1cd59d6

--- pyrefly/lib/alt/class/class_metadata.rs
+++ pyrefly/lib/alt/class/class_metadata.rs
@@ -75,6 +75,116 @@ impl BaseClass {
 }
 
 impl<'a, Ans: LookupAnswer> AnswersSolver<'a, Ans> {
+    pub fn class_tparams(
+        &self,
+        name: &Identifier,
+        scoped_tparams: Vec<TParam>,
+        bases: Vec<BaseClass>,
+        legacy: &[Idx<KeyLegacyTypeParam>],
+        errors: &ErrorCollector,
+    ) -> Vec<TParam> {
+        let legacy_tparams = legacy
+            .iter()
+            .filter_map(|key| self.get_idx(*key).deref().parameter().cloned())
+            .collect::<SmallSet<_>>();
+        let legacy_map = legacy_tparams
+            .iter()
+            .map(|p| (p.quantified.clone(), p))
+            .collect::<SmallMap<_, _>>();
+
+        let lookup_tparam = |t: &Type| {
+            let (q, kind) = match t {
+                Type::Unpack(t) => (t.as_quantified(), "TypeVarTuple"),
+                _ => (t.as_quantified(), "type variable"),
+            };
+            if q.is_none() && !matches!(t, Type::Any(AnyStyle::Error)) {
+                self.error(
+                    errors,
+                    name.range,
+                    ErrorKind::InvalidTypeVar,
+                    None,
+                    format!("Expected a {kind}, got `{}`", self.for_display(t.clone())),
+                );
+            }
+            q.and_then(|q| {
+                let p = legacy_map.get(&q);
+                if p.is_none() {
+                    self.error(
+                        errors,
+                        name.range,
+                        ErrorKind::InvalidTypeVar,
+                        None,
+                        "Redundant type parameter declaration".to_owned(),
+                    );
+                }
+                p.map(|x| (*x).clone())
+            })
+        };
+
+        // TODO(stroxler): There are a lot of checks, such as that `Generic` only appears once
+        // and no non-type-vars are used, that we can more easily detect in a dedictated class
+        // validation step that validates all the bases. We are deferring these for now.
+        let mut generic_tparams = SmallSet::new();
+        let mut protocol_tparams = SmallSet::new();
+        for base in bases.iter() {
+            match base {
+                BaseClass::Generic(ts) => {
+                    for t in ts {
+                        if let Some(p) = lookup_tparam(t) {
+                            generic_tparams.insert(p);
+                        }
+                    }
+                }
+                BaseClass::Protocol(ts) if !ts.is_empty() => {
+                    for t in ts {
+                        if let Some(p) = lookup_tparam(t) {
+                            protocol_tparams.insert(p);
+                        }
+                    }
+                }
+                _ => {}
+            }
+        }
+        if !generic_tparams.is_empty() && !protocol_tparams.is_empty() {
+            self.error(
+                errors,
+                name.range,
+                ErrorKind::InvalidInheritance,
+                None,
+                format!(
+                    "Class `{}` specifies type parameters in both `Generic` and `Protocol` bases",
+                    name.id,
+                ),
+            );
+        }
+        // Initialized the tparams: combine scoped and explicit type parameters
+        let mut tparams = SmallSet::new();
+        tparams.extend(scoped_tparams);
+        tparams.extend(generic_tparams);
+        tparams.extend(protocol_tparams);
+        // Handle implicit tparams: if a Quantified was bound at this scope and is not yet
+        // in tparams, we add it. These will be added in left-to-right order.
+        let implicit_tparams_okay = tparams.is_empty();
+        for p in legacy_tparams.iter() {
+            if !tparams.contains(p) {
+                if !implicit_tparams_okay {
+                    self.error(errors,
+                        name.range,
+                        ErrorKind::InvalidTypeVar,
+                        None,
+                        format!(
+                            "Class `{}` uses type variables not specified in `Generic` or `Protocol` base",
+                            name.id,
+                        ),
+                    );
+                }
+                tparams.insert(p.clone());
+            }
+        }
+
+        tparams.into_iter().collect()
+    }
+
     fn new_type_base(
         &self,
         base_type_and_range: Option<(Type, TextRange)>,
@@ -567,116 +677,6 @@ impl<'a, Ans: LookupAnswer> AnswersSolver<'a, Ans> {
             // This branch handles all other base classes.
             BaseClass::Expr(base_expr.clone())
         }
-    }
-
-    pub fn class_tparams(
-        &self,
-        name: &Identifier,
-        scoped_tparams: Vec<TParam>,
-        bases: Vec<BaseClass>,
-        legacy: &[Idx<KeyLegacyTypeParam>],
-        errors: &ErrorCollector,
-    ) -> Vec<TParam> {
-        let legacy_tparams = legacy
-            .iter()
-            .filter_map(|key| self.get_idx(*key).deref().parameter().cloned())
-            .collect::<SmallSet<_>>();
-        let legacy_map = legacy_tparams
-            .iter()
-            .map(|p| (p.quantified.clone(), p))
-            .collect::<SmallMap<_, _>>();
-
-        let lookup_tparam = |t: &Type| {
-            let (q, kind) = match t {
-                Type::Unpack(t) => (t.as_quantified(), "TypeVarTuple"),
-                _ => (t.as_quantified(), "type variable"),
-            };
-            if q.is_none() && !matches!(t, Type::Any(AnyStyle::Error)) {
-                self.error(
-                    errors,
-                    name.range,
-                    ErrorKind::InvalidTypeVar,
-                    None,
-                    format!("Expected a {kind}, got `{}`", self.for_display(t.clone())),
-                );
-            }
-            q.and_then(|q| {
-                let p = legacy_map.get(&q);
-                if p.is_none() {
-                    self.error(
-                        errors,
-                        name.range,
-                        ErrorKind::InvalidTypeVar,
-                        None,
-                        "Redundant type parameter declaration".to_owned(),
-                    );
-                }
-                p.map(|x| (*x).clone())
-            })
-        };
-
-        // TODO(stroxler): There are a lot of checks, such as that `Generic` only appears once
-        // and no non-type-vars are used, that we can more easily detect in a dedictated class
-        // validation step that validates all the bases. We are deferring these for now.
-        let mut generic_tparams = SmallSet::new();
-        let mut protocol_tparams = SmallSet::new();
-        for base in bases.iter() {
-            match base {
-                BaseClass::Generic(ts) => {
-                    for t in ts {
-                        if let Some(p) = lookup_tparam(t) {
-                            generic_tparams.insert(p);
-                        }
-                    }
-                }
-                BaseClass::Protocol(ts) if !ts.is_empty() => {
-                    for t in ts {
-                        if let Some(p) = lookup_tparam(t) {
-                            protocol_tparams.insert(p);
-                        }
-                    }
-                }
-                _ => {}
-            }
-        }
-        if !generic_tparams.is_empty() && !protocol_tparams.is_empty() {
-            self.error(
-                errors,
-                name.range,
-                ErrorKind::InvalidInheritance,
-                None,
-                format!(
-                    "Class `{}` specifies type parameters in both `Generic` and `Protocol` bases",
-                    name.id,
-                ),
-            );
-        }
-        // Initialized the tparams: combine scoped and explicit type parameters
-        let mut tparams = SmallSet::new();
-        tparams.extend(scoped_tparams);
-        tparams.extend(generic_tparams);
-        tparams.extend(protocol_tparams);
-        // Handle implicit tparams: if a Quantified was bound at this scope and is not yet
-        // in tparams, we add it. These will be added in left-to-right order.
-        let implicit_tparams_okay = tparams.is_empty();
-        for p in legacy_tparams.iter() {
-            if !tparams.contains(p) {
-                if !implicit_tparams_okay {
-                    self.error(errors,
-                        name.range,
-                        ErrorKind::InvalidTypeVar,
-                        None,
-                        format!(
-                            "Class `{}` uses type variables not specified in `Generic` or `Protocol` base",
-                            name.id,
-                        ),
-                    );
-                }
-                tparams.insert(p.clone());
-            }
-        }
-
-        tparams.into_iter().collect()
     }
 
     fn calculate_metaclass(
