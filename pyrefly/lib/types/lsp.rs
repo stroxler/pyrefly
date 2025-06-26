c724f1928efd39f3a389d67f4db868478fc5622a
Neil Mitchell <ndmitchell@meta.com>
2025-06-26 01:22:59 -0700

    Inline various lsp conversions
    Summary: Now they all have types that could be implemented correctly. They aren't implemented correctly though.
    
    Reviewed By: SamChou19815
    
    Differential Revision: D77299772
    
    fbshipit-source-id: 77224025562a7d4db9c01e1beb88710a43f9dba0

--- pyrefly/lib/types/lsp.rs
+++ pyrefly/lib/types/lsp.rs
@@ -1,29 +1,20 @@
 use pyrefly_util::lined_buffer::LinedBuffer;
-use pyrefly_util::lined_buffer::UserPos;
-use pyrefly_util::lined_buffer::UserRange;
 use ruff_text_size::TextRange;
 use ruff_text_size::TextSize;
 
-fn user_range_to_range(x: &UserRange) -> lsp_types::Range {
+pub fn text_range_to_range(info: &LinedBuffer, x: TextRange) -> lsp_types::Range {
     lsp_types::Range::new(
-        source_location_to_position(&x.start),
-        source_location_to_position(&x.end),
+        text_size_to_position(info, x.start()),
+        text_size_to_position(info, x.end()),
     )
 }
 
-pub fn text_range_to_range(info: &LinedBuffer, x: TextRange) -> lsp_types::Range {
-    user_range_to_range(&info.user_range(x))
-}
-
-fn source_location_to_position(x: &UserPos) -> lsp_types::Position {
-    lsp_types::Position {
-        line: x.line.to_zero_indexed() as u32,
-        character: x.column.to_zero_indexed() as u32,
-    }
-}
-
 pub fn text_size_to_position(info: &LinedBuffer, x: TextSize) -> lsp_types::Position {
-    source_location_to_position(&info.user_pos(x))
+    let user_pos = info.user_pos(x);
+    lsp_types::Position {
+        line: user_pos.line.to_zero_indexed() as u32,
+        character: user_pos.column.to_zero_indexed() as u32,
+    }
 }
 
 pub fn position_to_text_size(info: &LinedBuffer, position: lsp_types::Position) -> TextSize {
