7ecf94a0dda737a91e5670b006a57e1bf3c7a3ee
Conner Nilsen <connernilsen@meta.com>
2025-07-08 11:08:03 -0700

    Create ConfigOrigin enum to track where specific configs come from
    Summary:
    # This Diff
    
    This diff adds a utility enum for tracking the source of a config value. I originally created it a while ago before deciding `SitePackagePathSource` was easier to do, but I have more fields that we need to keep track of the source of, so I'm actually gonna add it now.
    
    This enum is a transparent value (which I think is kinda cool!). You can parse it into a value of its type without specifying it. Example:
    ```
    // the struct
    struct Test {
      a: Option<ConfigOption<String>>
    }
    // the toml file
    [test]
    a = "hello!"
    
    // parsing it would come out as this:
    Test { a: Some(ConfigOption::ConfigFile("hello!")) }
    ```
    
    You can also serialize it without outputting the `ConfigOption` wrapper, and we're able to avoid serializing non-`ConfigFile` variants, allowing us to avoid dumping out values that are automatically configured that we might want to hide.
    
    When it comes from anywhere else, we can explicitly set the value to something else (e.g. when we get a value from CLAP, we can set it on the config with another `ConfigOption` variant).
    
    I also add in some helper functions around it similar to an `Option` type, which get used in upcoming diffs.
    
    # Diff Stack
    
    The upcoming diff stack fixes a bug with how we're finding and using interpreters. The way that it works now is actually slightly different from what the online documentation says, and now that I've had more time to think about it, I want it to behave slightly differently.
    
    The next few diffs will fix the ordering and reorganize the logic around interpreter finding to be a bit cleaner.
    
    Reviewed By: ndmitchell
    
    Differential Revision: D77703777
    
    fbshipit-source-id: 8771c54a0cde4de5930176944bb3a930ef58dd1c

--- pyrefly/lib/config/util.rs
+++ pyrefly/lib/config/util.rs
@@ -5,6 +5,10 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+use std::fmt::Display;
+use std::ops::Deref;
+use std::ops::DerefMut;
+
 use serde::Deserialize;
 use serde::Serialize;
 use toml::Table;
@@ -56,13 +60,166 @@ impl PyProject {
 pub fn skip_default_true(v: &bool) -> bool {
     *v
 }
-///
+
 /// Used in serde's skip_serializing_if attribute to skip serializing a boolean field that defaults to true.
 #[allow(clippy::trivially_copy_pass_by_ref)]
 pub fn skip_default_false(v: &bool) -> bool {
     !*v
 }
 
+/// Used in serde's skip_serializing_if attribute to skip serializing a boolean field that defaults to false.
 pub fn none_or_empty<T>(v: &Option<Vec<T>>) -> bool {
     v.as_ref().is_none_or(|v| v.is_empty())
+}
+
+/// A helper struct for detailing the origin of a config option.
+/// The `Serialize` functionality enables us to only serialize [`ConfigOrigin::ConfigFile`]
+/// values, skipping serialization for values we might be able to figure out automatically,
+/// and we deserialize directly into a `ConfigFile` variant, making it easy to
+/// update with a different `Auto` or `CommandLine` variant when overriding it.
+#[derive(Debug, PartialEq, Eq, Deserialize, Clone, Copy)]
+#[serde(untagged)]
+pub enum ConfigOrigin<T> {
+    /// This value was explicitly provided from a CLI flag.
+    #[serde(skip)]
+    CommandLine(T),
+
+    /// This value was automatically constructed by Pyrefly.
+    #[serde(skip)]
+    Auto(T),
+
+    /// This value was provided by a [`ConfigFile`], either explicitly or implicitly.
+    ConfigFile(T),
+}
+
+impl<T: Default> Default for ConfigOrigin<T> {
+    fn default() -> Self {
+        ConfigOrigin::ConfigFile(T::default())
+    }
+}
+
+impl<T: Serialize> Serialize for ConfigOrigin<T> {
+    /// Serialize this `ConfigOrigin`'s internal value, making the `ConfigOrigin`
+    /// transparent. This will serialize ALL `ConfigOrigin` values,
+    /// so you must use `skip_serializing_if` helpers below to better control
+    /// when to output a value.
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        self.deref().serialize(serializer)
+    }
+}
+
+impl<T: Display> Display for ConfigOrigin<T> {
+    /// Display this `ConfigOrigin`'s internal value, making the `ConfigOrigin`
+    /// transparent.
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.deref().fmt(f)
+    }
+}
+
+impl<T> Deref for ConfigOrigin<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        match self {
+            Self::CommandLine(value) | Self::ConfigFile(value) | Self::Auto(value) => value,
+        }
+    }
+}
+
+impl<T> DerefMut for ConfigOrigin<T> {
+    #[allow(unused)]
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        match self {
+            Self::CommandLine(value) | Self::ConfigFile(value) | Self::Auto(value) => value,
+        }
+    }
+}
+
+impl<T: Deref> ConfigOrigin<T> {
+    #[allow(unused)]
+    pub fn as_deref(&self) -> ConfigOrigin<&<T as Deref>::Target> {
+        self.as_ref().map(|t| t.deref())
+    }
+}
+
+impl<T> ConfigOrigin<T> {
+    /// Construct a new [`ConfigOrigin::CommandLine`] with the given value.
+    #[allow(unused)]
+    pub fn cli(value: T) -> Self {
+        Self::CommandLine(value)
+    }
+
+    /// Construct a new [`ConfigOrigin::ConfigFile`] with the given value.
+    #[allow(unused)]
+    pub fn config(value: T) -> Self {
+        Self::ConfigFile(value)
+    }
+
+    /// Construct a new [`ConfigOrigin::Auto`] with the given value.
+    #[allow(unused)]
+    pub fn auto(value: T) -> Self {
+        Self::Auto(value)
+    }
+
+    /// Consume the [`ConfigOrigin`], returning the contained value.
+    #[allow(unused)]
+    pub fn get(self) -> T {
+        match self {
+            Self::CommandLine(value) | Self::ConfigFile(value) | Self::Auto(value) => value,
+        }
+    }
+
+    /// Consume the [`ConfigOrigin`], mapping the internal value with the
+    /// given function, and inserting it into a new [`ConfigOrigin`] of the same
+    /// variant.
+    #[allow(unused)]
+    pub fn map<U, F>(self, f: F) -> ConfigOrigin<U>
+    where
+        F: FnOnce(T) -> U,
+    {
+        match self {
+            ConfigOrigin::ConfigFile(value) => ConfigOrigin::ConfigFile(f(value)),
+            ConfigOrigin::CommandLine(value) => ConfigOrigin::CommandLine(f(value)),
+            ConfigOrigin::Auto(value) => ConfigOrigin::Auto(f(value)),
+        }
+    }
+
+    #[allow(unused)]
+    fn as_ref(&self) -> ConfigOrigin<&T> {
+        match *self {
+            ConfigOrigin::ConfigFile(ref value) => ConfigOrigin::ConfigFile(value),
+            ConfigOrigin::CommandLine(ref value) => ConfigOrigin::CommandLine(value),
+            ConfigOrigin::Auto(ref value) => ConfigOrigin::Auto(value),
+        }
+    }
+
+    /// Determine if this [`Option<ConfigOrigin>`] should bee output when serializing.
+    /// We only serialize if the value is `Some(ConfigFile)`. All other
+    /// [`Option`] and [`ConfigOrigin`] variants are not serialized.
+    #[allow(unused)]
+    pub fn should_skip_serializing_option(origin: &Option<Self>) -> bool {
+        match origin {
+            Some(Self::ConfigFile(_)) => false,
+            _ => true,
+        }
+    }
+}
+
+impl<T, E> ConfigOrigin<Result<T, E>> {
+    /// Swap a ConfigOrigin<Result<T, E>>'s [`ConfigOrigin`] and [`Result`] types,
+    /// turning it into a `Result<ConfigOrigin<T>, E>`.
+    #[allow(unused)]
+    pub fn transpose_err(self) -> Result<ConfigOrigin<T>, E> {
+        match self {
+            ConfigOrigin::ConfigFile(Ok(value)) => Ok(ConfigOrigin::ConfigFile(value)),
+            ConfigOrigin::CommandLine(Ok(value)) => Ok(ConfigOrigin::CommandLine(value)),
+            ConfigOrigin::Auto(Ok(value)) => Ok(ConfigOrigin::Auto(value)),
+            ConfigOrigin::ConfigFile(Err(err))
+            | ConfigOrigin::CommandLine(Err(err))
+            | ConfigOrigin::Auto(Err(err)) => Err(err),
+        }
+    }
 }
