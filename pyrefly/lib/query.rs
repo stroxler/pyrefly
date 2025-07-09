1c18ae2cd7396362c865606d83f95131eb94c77a
Neil Mitchell <ndmitchell@meta.com>
2025-07-09 11:00:01 -0700

    Implement is_subset
    Summary: Tries to figure out if two types are a subset of each other.
    
    Reviewed By: stroxler
    
    Differential Revision: D78011815
    
    fbshipit-source-id: 7b4891d565e2403578d448615a1b23ed770f6028

--- pyrefly/lib/query.rs
+++ pyrefly/lib/query.rs
@@ -8,8 +8,11 @@
 //! Query interface for pyrefly. Just experimenting for the moment - not intended for external use.
 
 use std::io::Cursor;
+use std::path::PathBuf;
+use std::sync::Arc;
 
 use dupe::Dupe;
+use pyrefly_python::ast::Ast;
 use pyrefly_python::module_name::ModuleName;
 use pyrefly_python::module_path::ModulePath;
 use pyrefly_python::sys_info::SysInfo;
@@ -20,6 +23,9 @@ use pyrefly_util::prelude::SliceExt;
 use pyrefly_util::prelude::VecExt;
 use pyrefly_util::visit::Visit;
 use ruff_python_ast::Expr;
+use ruff_python_ast::ExprAttribute;
+use ruff_python_ast::ModModule;
+use ruff_python_ast::name::Name;
 use ruff_text_size::Ranged;
 use starlark_map::small_set::SmallSet;
 
@@ -131,5 +137,72 @@ impl Query {
 
         ast.visit(&mut |x| f(x, &module_info, &answers, &mut res));
         Some(res)
+    }
+
+    /// Given an expression, which contains qualified types, guess which imports to add.
+    ///
+    /// For example `foo.bar.baz` will return `[foo.bar]`.
+    fn find_imports(x: &ModModule) -> Vec<String> {
+        fn g(x: &ExprAttribute) -> Option<Vec<&Name>> {
+            match &*x.value {
+                Expr::Attribute(x) => {
+                    let mut res = g(x)?;
+                    res.push(&x.attr.id);
+                    Some(res)
+                }
+                Expr::Name(x) => Some(vec![&x.id]),
+                _ => None,
+            }
+        }
+
+        fn f(x: &Expr, res: &mut SmallSet<String>) {
+            if let Expr::Attribute(x) = x {
+                if let Some(module) = g(x) {
+                    res.insert(module.map(|x| x.as_str()).join("."));
+                }
+            } else {
+                x.recurse(&mut |x| f(x, res));
+            }
+        }
+        let mut res = SmallSet::new();
+        x.visit(&mut |x| f(x, &mut res));
+        res.into_iter().collect()
+    }
+
+    /// Return `Err` if you can't resolve them to types, otherwise return `lt <: gt`.
+    pub fn is_subtype(
+        &self,
+        name: ModuleName,
+        path: PathBuf,
+        lt: &str,
+        gt: &str,
+    ) -> Result<bool, String> {
+        let types = format!("type pyrefly_lt = ({lt})\ntype pyrefly_gt = ({gt})\n");
+        let imported = Self::find_imports(&Ast::parse(&types).0);
+        let imports = imported.map(|x| format!("import {x}\n")).join("");
+        let check = "def pyrefly_func(x: pyrefly_lt) -> pyrefly_gt:\n    return x";
+
+        let before = format!("{imports}\n{types}\n");
+        let after = format!("{imports}\n{types}\n{check}");
+
+        let mut t = self.state.transaction();
+        let h = self.make_handle(name, ModulePath::memory(path.clone()));
+        t.set_memory(vec![(path.clone(), Some(Arc::new(before.clone())))]);
+        t.run(&[(h.dupe(), Require::Everything)]);
+        let errors = t.get_errors([&h]).collect_errors();
+        if !errors.shown.is_empty() {
+            let mut res = Vec::new();
+            for e in errors.shown {
+                e.write_line(&mut Cursor::new(&mut res), true).unwrap();
+            }
+            return Err(format!(
+                "Errors from is_subtype `{lt}` <: `{gt}`\n{}\n\nSource code:\n{before}",
+                str::from_utf8(&res).unwrap_or("UTF8 error")
+            ));
+        }
+        t.set_memory(vec![(path.clone(), Some(Arc::new(after)))]);
+        t.run(&[(h.dupe(), Require::Everything)]);
+        let errors = t.get_errors([&h]).collect_errors();
+        Ok(errors.shown.is_empty())
     }
 }
