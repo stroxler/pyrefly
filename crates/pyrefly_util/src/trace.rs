1b649250a5605226e9715d68469942b220ba7d3b
Neil Mitchell <ndmitchell@meta.com>
2025-07-17 15:07:50 -0700

    Refactor trace to produce a layer
    Summary: Useful if we want to integrate with another trace consumer. Clearer code.
    
    Reviewed By: grievejia
    
    Differential Revision: D78484513
    
    fbshipit-source-id: a2202d6560b8a43f0af29140b253c9f9d2b34ab6

--- crates/pyrefly_util/src/trace.rs
+++ crates/pyrefly_util/src/trace.rs
@@ -11,10 +11,41 @@ use std::sync::Once;
 
 use anstream::stderr;
 use tracing_subscriber::Layer;
+use tracing_subscriber::Registry;
 use tracing_subscriber::filter::EnvFilter;
 use tracing_subscriber::filter::LevelFilter;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
+
+pub type TracingLayer = Box<dyn Layer<Registry> + Send + Sync>;
+
+/// Create a layer for user tracing.
+pub fn tracing_layer(verbose: bool, testing: bool) -> TracingLayer {
+    const ENV_VAR: &str = "PYREFLY_LOG";
+    let mut env_filter = EnvFilter::from_env(ENV_VAR);
+    if std::env::var_os(ENV_VAR).is_none() {
+        // Enable info log by default
+        env_filter = env_filter.add_directive(if verbose {
+            LevelFilter::DEBUG.into()
+        } else {
+            LevelFilter::INFO.into()
+        });
+    }
+
+    let layer = tracing_subscriber::fmt::layer()
+        .with_line_number(false)
+        .with_file(false)
+        .without_time()
+        .with_writer(stderr)
+        .with_target(false);
+
+    if testing {
+        // The with_test_writer causes us to write to stdout, not stderr.
+        layer.with_test_writer().with_filter(env_filter).boxed()
+    } else {
+        layer.with_filter(env_filter).boxed()
+    }
+}
 
 /// Set up tracing so it prints to stderr, and can be used for output.
 /// Most things should use `info` and `debug` level for showing messages.
@@ -24,33 +55,8 @@ pub fn init_tracing(verbose: bool, testing: bool) {
     static INIT_TRACING_ONCE: Once = Once::new();
 
     INIT_TRACING_ONCE.call_once(|| {
-        const ENV_VAR: &str = "PYREFLY_LOG";
-        let mut env_filter = EnvFilter::from_env(ENV_VAR);
-        if std::env::var_os(ENV_VAR).is_none() {
-            // Enable info log by default
-            env_filter = env_filter.add_directive(if verbose {
-                LevelFilter::DEBUG.into()
-            } else {
-                LevelFilter::INFO.into()
-            });
-        }
-
-        let layer = tracing_subscriber::fmt::layer()
-            .with_line_number(false)
-            .with_file(false)
-            .without_time()
-            .with_writer(stderr)
-            .with_target(false);
-
-        if testing {
-            // The with_test_writer causes us to write to stdout, not stderr.
-            tracing_subscriber::registry()
-                .with(layer.with_test_writer().with_filter(env_filter))
-                .init();
-        } else {
-            tracing_subscriber::registry()
-                .with(layer.with_filter(env_filter))
-                .init();
-        }
+        tracing_subscriber::registry()
+            .with(tracing_layer(verbose, testing))
+            .init();
     })
 }
