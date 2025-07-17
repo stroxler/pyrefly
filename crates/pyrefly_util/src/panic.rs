6c2d3e78ec14e70d5558490f195454bfb1e82504
Neil Mitchell <ndmitchell@meta.com>
2025-07-17 11:08:29 -0700

    Only print at most one panic
    Reviewed By: stroxler
    
    Differential Revision: D78484509
    
    fbshipit-source-id: b76e007db9cfb25ed1e67bd4c14742eeeaf5d232

--- crates/pyrefly_util/src/panic.rs
+++ crates/pyrefly_util/src/panic.rs
@@ -6,21 +6,28 @@
  */
 
 use std::backtrace::Backtrace;
+use std::sync::Once;
 
 use tracing::error;
 
 pub fn exit_on_panic() {
+    // Sometimes we get two simultaneous panics, and there output gets co-mingled.
+    // Make sure we only report one panic.
+    static PANIC_LOCK: Once = Once::new();
+
     std::panic::set_hook(Box::new(move |info| {
-        error!(
-            "Thread panicked, shutting down: {info}\nBacktrace:\n{}",
-            Backtrace::force_capture()
-        );
-        eprintln!("Sorry, Pyrefly crashed, this is always a bug in Pyrefly itself.");
-        if cfg!(fbcode_build) {
-            eprintln!("Please report the bug at https://fb.workplace.com/groups/pyreqa");
-        } else {
-            eprintln!("Please report the bug at https://github.com/facebook/pyrefly/issues/new")
-        }
+        PANIC_LOCK.call_once(|| {
+            error!(
+                "Thread panicked, shutting down: {info}\nBacktrace:\n{}",
+                Backtrace::force_capture()
+            );
+            eprintln!("Sorry, Pyrefly crashed, this is always a bug in Pyrefly itself.");
+            if cfg!(fbcode_build) {
+                eprintln!("Please report the bug at https://fb.workplace.com/groups/pyreqa");
+            } else {
+                eprintln!("Please report the bug at https://github.com/facebook/pyrefly/issues/new")
+            }
+        });
         std::process::exit(1);
     }));
 }
