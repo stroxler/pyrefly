58235e7b73e25ba3a8fc66193f6d48cb1f07a8e4
Maxime Arthaud <arthaud@meta.com>
2025-07-22 11:22:50 -0700

    Add class information in pysa report files
    Summary:
    Pysa needs information about classes, their parents, fields, etc.
    For now, let's just export classes and their parents.
    
    Reviewed By: stroxler
    
    Differential Revision: D78738356
    
    fbshipit-source-id: 1e67f67c4c7a1f47c358650c255919b72298d4cb

--- pyrefly/lib/report/pysa/mod.rs
+++ pyrefly/lib/report/pysa/mod.rs
@@ -12,11 +12,16 @@ use std::path::Path;
 use std::path::PathBuf;
 use std::time::Instant;
 
+use itertools::Itertools;
 use pyrefly_python::ast::Ast;
+use pyrefly_python::module::Module;
 use pyrefly_python::module_name::ModuleName;
 use pyrefly_python::module_path::ModulePath;
 use pyrefly_python::module_path::ModulePathDetails;
 use pyrefly_python::symbol_kind::SymbolKind;
+use pyrefly_types::class::Class;
+use pyrefly_types::class::ClassDefIndex;
+use pyrefly_types::types::Type;
 use pyrefly_util::fs_anyhow;
 use pyrefly_util::lined_buffer::DisplayRange;
 use pyrefly_util::visit::Visit;
@@ -24,12 +29,15 @@ use rayon::prelude::*;
 use ruff_python_ast::Expr;
 use ruff_python_ast::ExprContext;
 use ruff_python_ast::Stmt;
+use ruff_python_ast::StmtClassDef;
 use ruff_text_size::Ranged;
 use serde::Serialize;
 use tracing::debug;
 use tracing::info;
 
 use crate::alt::answers::Answers;
+use crate::binding::binding::KeyClassMetadata;
+use crate::binding::bindings::Bindings;
 use crate::module::module_info::ModuleInfo;
 use crate::module::typeshed::typeshed;
 use crate::state::handle::Handle;
@@ -42,6 +50,19 @@ use crate::types::stdlib::Stdlib;
 /// Represents a unique identifier for a module
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord, Serialize)]
 struct ModuleId(u32);
+
+/// Represents a unique identifier for a class, inside a module
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord, Serialize)]
+struct ClassId(u32);
+
+impl ClassId {
+    fn from_index(index: ClassDefIndex) -> ClassId {
+        ClassId(index.0)
+    }
+    fn from_class(class: &Class) -> ClassId {
+        ClassId(class.index().0)
+    }
+}
 
 #[derive(Debug, Clone, Serialize)]
 struct PysaProjectModule {
@@ -56,6 +77,8 @@ struct PysaProjectModule {
 struct PysaProjectFile {
     format_version: u32,
     modules: HashMap<ModuleId, PysaProjectModule>,
+    builtin_module_id: ModuleId,
+    object_class_id: ClassId,
 }
 
 #[derive(Debug, Clone, Serialize)]
@@ -64,6 +87,21 @@ struct DefinitionRef {
     module_name: String, // For debugging purposes only. Reader should use the module id.
     location: String,
     identifier: String,
+}
+
+#[derive(Debug, Clone, Serialize)]
+struct ClassRef {
+    module_id: ModuleId,
+    module_name: String, // For debugging purposes only. Reader should use the module id.
+    class_id: ClassId,
+    class_name: String, // For debugging purposes only. Reader should use the class id.
+}
+
+#[derive(Debug, Clone, Serialize)]
+struct ClassDefinition {
+    class_id: ClassId,
+    name: String,
+    bases: Vec<ClassRef>,
 }
 
 /// Format of a module file `my.module:id.json`
@@ -76,6 +114,7 @@ struct PysaModuleFile {
     source_path: ModulePathDetails,
     type_of_expression: HashMap<String, String>,
     goto_definitions_of_expression: HashMap<String, Vec<DefinitionRef>>,
+    class_definitions: HashMap<String, ClassDefinition>,
 }
 
 /// Represents what makes a module unique
@@ -93,10 +132,17 @@ impl ModuleKey {
         }
     }
 
-    fn from_info(module_info: &ModuleInfo) -> ModuleKey {
+    fn from_module_info(module_info: &ModuleInfo) -> ModuleKey {
         ModuleKey {
             name: module_info.name(),
             path: module_info.path().clone(),
+        }
+    }
+
+    fn from_module(module: &Module) -> ModuleKey {
+        ModuleKey {
+            name: module.name(),
+            path: module.path().clone(),
         }
     }
 }
@@ -141,11 +187,13 @@ struct VisitorContext<'a> {
     handle: &'a Handle,
     module_ids: &'a ModuleIds,
     module_info: &'a ModuleInfo,
+    bindings: &'a Bindings,
     answers: &'a Answers,
     stdlib: &'a Stdlib,
     transaction: &'a Transaction<'a>,
     type_of_expression: &'a mut HashMap<String, String>,
     definitions_of_expression: &'a mut HashMap<String, Vec<DefinitionRef>>,
+    class_definitions: &'a mut HashMap<String, ClassDefinition>,
 }
 
 fn add_expression_definitions(
@@ -166,7 +214,10 @@ fn add_expression_definitions(
         .filter_map(|definition| {
             let module_info = &definition.module;
             let display_range = module_info.display_range(definition.definition_range);
-            match context.module_ids.get(ModuleKey::from_info(module_info)) {
+            match context
+                .module_ids
+                .get(ModuleKey::from_module_info(module_info))
+            {
                 Some(module_id) => Some(DefinitionRef {
                     module_id,
                     module_name: module_info.name().to_string(),
@@ -260,6 +311,56 @@ fn visit_expression(e: &Expr, context: &mut VisitorContext) {
     e.recurse(&mut |e| visit_expression(e, context));
 }
 
+fn visit_class_definition(class_def: &StmtClassDef, context: &mut VisitorContext) {
+    let class = context
+        .bindings
+        .definition_at_position(class_def.name.range().start())
+        .map(|k| context.bindings.key_to_idx(k))
+        .and_then(|idx| context.answers.get_idx(idx))
+        .and_then(|type_info| match type_info.ty() {
+            Type::ClassDef(class) => Some(class.clone()),
+            _ => None,
+        });
+
+    if let Some(class) = class {
+        let display_range = context.module_info.display_range(class_def.range());
+        let class_index = class.index();
+        let metadata = context
+            .answers
+            .get_idx(context.bindings.key_to_idx(&KeyClassMetadata(class_index)))
+            .unwrap();
+
+        let class_definition = ClassDefinition {
+            class_id: ClassId::from_class(&class),
+            name: class.qname().id().to_string(),
+            bases: metadata
+                .bases_with_metadata()
+                .iter()
+                .map(|(class_type, _)| {
+                    let base_class = class_type.class_object();
+                    ClassRef {
+                        module_id: context
+                            .module_ids
+                            .get(ModuleKey::from_module(base_class.module()))
+                            .unwrap(),
+                        module_name: base_class.module_name().to_string(),
+                        class_id: ClassId::from_class(base_class),
+                        class_name: base_class.qname().id().to_string(),
+                    }
+                })
+                .collect::<Vec<_>>(),
+        };
+
+        assert!(
+            context
+                .class_definitions
+                .insert(location_key(&display_range), class_definition)
+                .is_none(),
+            "Found class definitions with the same location"
+        );
+    }
+}
+
 fn visit_statement(stmt: &Stmt, context: &mut VisitorContext) {
     match stmt {
         Stmt::FunctionDef(function_def) => {
@@ -297,6 +398,7 @@ fn visit_statement(stmt: &Stmt, context: &mut VisitorContext) {
             visit_statements(function_def.body.iter(), context);
         }
         Stmt::ClassDef(class_def) => {
+            visit_class_definition(class_def, context);
             visit_expressions(
                 class_def
                     .decorator_list
@@ -418,11 +520,13 @@ fn get_module_file(
     let module_info = &transaction.get_module_info(handle).unwrap();
 
     let ast = &*transaction.get_ast(handle).unwrap();
+    let bindings = &transaction.get_bindings(handle).unwrap();
     let answers = &*transaction.get_answers(handle).unwrap();
     let stdlib = &*transaction.get_stdlib(handle);
 
     let mut type_of_expression = HashMap::new();
     let mut definitions_of_expression = HashMap::new();
+    let mut class_definitions = HashMap::new();
 
     for stmt in &ast.body {
         visit_statement(
@@ -431,11 +535,13 @@ fn get_module_file(
                 handle,
                 module_ids,
                 module_info,
+                bindings,
                 answers,
                 stdlib,
                 transaction,
                 type_of_expression: &mut type_of_expression,
                 definitions_of_expression: &mut definitions_of_expression,
+                class_definitions: &mut class_definitions,
             },
         );
     }
@@ -447,6 +553,7 @@ fn get_module_file(
         source_path: module_info.path().details().clone(),
         type_of_expression,
         goto_definitions_of_expression: definitions_of_expression,
+        class_definitions,
     }
 }
 
@@ -535,12 +642,28 @@ pub fn write_results(results_directory: &Path, transaction: &Transaction) -> any
         fs_anyhow::write(&target_path, content.as_bytes())?;
     }
 
+    let builtin_module = handles
+        .iter()
+        .filter(|handle| handle.module().as_str() == "builtins")
+        .exactly_one()
+        .expect("expected exactly one builtins module");
+    let object_class_id = ClassId::from_class(
+        transaction
+            .get_stdlib(builtin_module)
+            .object()
+            .class_object(),
+    );
+
     let writer = BufWriter::new(File::create(results_directory.join("pyrefly.pysa.json"))?);
     serde_json::to_writer(
         writer,
         &PysaProjectFile {
             format_version: 1,
             modules: project_modules,
+            builtin_module_id: module_ids
+                .get(ModuleKey::from_handle(builtin_module))
+                .unwrap(),
+            object_class_id,
         },
     )?;
 
