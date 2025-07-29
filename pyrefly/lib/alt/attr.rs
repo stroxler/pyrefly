0ff97f730a35572f6155534d63a39ddf56676871
Steven Troxler <stroxler@meta.com>
2025-07-29 11:00:25 -0700

    Use simpler names
    Summary:
    While I was eliminating the unneeded `Attribute::new` I noticed these varnames
    that seemed both more verbose and less clear than they could be.
    
    Reviewed By: ndmitchell
    
    Differential Revision: D79181078
    
    fbshipit-source-id: 18ac115fefca93c556f61baf290fad1e8ca79aaa

--- pyrefly/lib/alt/attr.rs
+++ pyrefly/lib/alt/attr.rs
@@ -685,9 +685,9 @@ impl<'a, Ans: LookupAnswer> AnswersSolver<'a, Ans> {
             LookupResult::Found(setattr_attr) => {
                 let result = self
                     .resolve_get_access(setattr_attr, range, errors, context)
-                    .map(|setattr_attr_ty| {
+                    .map(|setattr_ty| {
                         self.call_setattr(
-                            setattr_attr_ty,
+                            setattr_ty,
                             CallArg::Arg(got),
                             attr_name.clone(),
                             range,
@@ -732,9 +732,9 @@ impl<'a, Ans: LookupAnswer> AnswersSolver<'a, Ans> {
             LookupResult::Found(delattr_attr) => {
                 let result = self
                     .resolve_get_access(delattr_attr, range, errors, context)
-                    .map(|delattr_attr_ty| {
+                    .map(|delattr_ty| {
                         self.call_getattr_or_delattr(
-                            delattr_attr_ty,
+                            delattr_ty,
                             attr_name.clone(),
                             range,
                             errors,
