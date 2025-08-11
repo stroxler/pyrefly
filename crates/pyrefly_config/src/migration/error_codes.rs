stroxler/pyrefly

commit 08215cc546c6e39e165b35bb9a08682eee8fa4e2

Expand error codes we pull from mypy (2/n)
Summary: We currently only pull one disabled error code during our init set - I'm going to chip away at this with a stack to ensure we're setting more defaults.

Reviewed By: yangdanny97

Differential Revision: D79909910

fbshipit-source-id: f7497c36af537d7d621e1fcd71e363eb1d440695

crates/pyrefly_config/src/migration/error_codes.rs
@@ -6,6 +6,7 @@
  */
 
 use configparser::ini::Ini;
+use configparser::ini::IniDefault;
 
 use crate::config::ConfigFile;
 use crate::migration::config_option_migrater::ConfigOptionMigrater;
@@ -51,6 +52,7 @@ impl ConfigOptionMigrater for ErrorCodes {
 
 #[cfg(test)]
 mod tests {
+
     use super::*;
     use crate::error_kind::ErrorKind;
     use crate::error_kind::Severity;
@@ -113,8 +115,15 @@ mod tests {
 
     #[test]
     fn test_migrate_from_mypy_with_disable_codes() {
-        let mut mypy_cfg = Ini::new();
-        mypy_cfg.set("mypy", "disable_error_code", Some("arg-type".to_owned()));
+        let mut default = IniDefault::default();
+        default.multiline = true;
+        let mut mypy_cfg = Ini::new_from_defaults(default);
+
+        mypy_cfg.set(
+            "mypy",
+            "disable_error_code",
+            Some("arg-type, call-overload".to_owned()),
+        );
 
         let mut pyrefly_cfg = ConfigFile::default();
 
@@ -125,6 +134,10 @@ mod tests {
         let errors = pyrefly_cfg.root.errors.as_ref().unwrap();
         assert_eq!(
             errors.severity(ErrorKind::BadArgumentType),
+            Severity::Ignore
+        );
+        assert_eq!(
+            errors.severity(ErrorKind::InvalidOverload),
             Severity::Ignore
         );
     }

crates/pyrefly_config/src/migration/mypy/util.rs
@@ -124,6 +124,10 @@ fn code_to_kind(mut errors: HashMap<String, Severity>) -> Option<ErrorDisplayCon
         map.insert(ErrorKind::BadArgumentType, value);
     }
 
+    if let Some(value) = [errors.remove("call-overload")].into_iter().flatten().max() {
+        map.insert(ErrorKind::NoMatchingOverload, value);
+    }
+
     if map.is_empty() {
         None
     } else {

commit 2f3d7d6b22352fe3596c3faa772a8861e690920b

Fix a failing test in migration/error_codes.rs (#886)
Summary:
Build is failing on main branch, seems to be introduced by 08215cc546c6e39e165b35bb9a08682eee8fa4e2/D79909910

Pull Request resolved: https://github.com/facebook/pyrefly/pull/886

Reviewed By: rchen152

Differential Revision: D80036658

Pulled By: fangyi-zhou

fbshipit-source-id: 49353f3e7cf1c5816ecffba22a9d59021db90e86

crates/pyrefly_config/src/migration/error_codes.rs
@@ -137,7 +137,7 @@ mod tests {
             Severity::Ignore
         );
         assert_eq!(
-            errors.severity(ErrorKind::InvalidOverload),
+            errors.severity(ErrorKind::NoMatchingOverload),
             Severity::Ignore
         );
     }