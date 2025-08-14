30bb2442ac160e555935c07ec26c17759aca1537
Rebecca Chen <rechen@meta.com>
2025-08-14 02:01:06 -0700

    Move a comment that accidentally wandered away from the line the comment is about
    Reviewed By: yangdanny97
    
    Differential Revision: D80221335
    
    fbshipit-source-id: 7456df679b80747f8a34735e1aa264c843ce6c08

--- pyrefly/lib/alt/call.rs
+++ pyrefly/lib/alt/call.rs
@@ -473,7 +473,6 @@ impl<'a, Ans: LookupAnswer> AnswersSolver<'a, Ans> {
         context: Option<&dyn Fn() -> ErrorContext>,
         hint: Option<HintRef>,
     ) -> Type {
-        // We know `__init__` exists because we synthesize it.
         if let Some(hint) = hint {
             self.solver()
                 .freshen_class_targs(typed_dict.targs_mut(), self.uniques);
@@ -482,6 +481,7 @@ impl<'a, Ans: LookupAnswer> AnswersSolver<'a, Ans> {
             self.solver().generalize_class_targs(typed_dict.targs_mut());
         }
         let hint = None; // discard hint
+        // We know `__init__` exists because we synthesize it.
         let init_method = self.get_typed_dict_dunder_init(&typed_dict).unwrap();
         self.call_infer(
             self.as_call_target_or_error(
