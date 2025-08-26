0548111be73956e6fe85de79ed94a516021202aa
Zeina Migeed <migeedz@meta.com>
2025-08-26 08:21:40 -0700

    Add ConfigDict extra unittests
    Summary:
    Interestingly, with ConfigDict, if the wrong "extra" keyword is provided, the program still runs, unlike in the class keyword case.
    
    I think we should alert the user in both cases. The stubs already do this for us in ConfigDict and I'm not sure we should change that.
    
    There's also the question of whether we should semantically default to extra = true/false in the wrong case, but I feel like it's not that relavant since the program is already problematic (hence to bug tag). I set it to true in the next diff though and will propagate the info to actually remove the error
    
    Reviewed By: stroxler
    
    Differential Revision: D80996984
    
    fbshipit-source-id: 51e18ee57274a60d35dfe3ad4ca0faa9305d5978

--- pyrefly/lib/test/pydantic/extra.rs
+++ pyrefly/lib/test/pydantic/extra.rs
@@ -94,3 +94,97 @@ class ModelForbid(BaseModel, extra="123"): # E: Invalid value for `extra`. Expec
 ModelForbid(x=1, y=2) # E: Unexpected keyword argument `y` in function `ModelForbid.__init__`
 "#,
 );
+
+testcase!(
+    bug = "y should be allowed because extra is allowed",
+    test_extra_allow_config_dict,
+    pydantic_env(),
+    r#"
+from pydantic import BaseModel, ConfigDict
+
+class ModelAllow(BaseModel):
+    ConfigDict(extra="allow")
+    x: int 
+
+ModelAllow(x=1, y=2) # E: Unexpected keyword argument `y` in function `ModelAllow.__init__` 
+
+"#,
+);
+
+testcase!(
+    test_extra_forbid_config_dict,
+    pydantic_env(),
+    r#"
+from pydantic import BaseModel, ConfigDict
+
+class ModelForbid(BaseModel):
+    ConfigDict(extra="forbid")
+    x: int
+
+ModelForbid(x=1, y=2) # E: Unexpected keyword argument `y` in function `ModelForbid.__init__`
+"#,
+);
+
+testcase!(
+    bug = "should not raise an error for ignore",
+    test_extra_ignore_config_dict,
+    pydantic_env(),
+    r#"
+from pydantic import BaseModel, ConfigDict
+
+class ModelForbid(BaseModel):
+    ConfigDict(extra="ignore")
+    x: int
+
+ModelForbid(x=1, y=2) # E: Unexpected keyword argument `y` in function `ModelForbid.__init__`
+"#,
+);
+
+testcase!(
+    bug = "should not raise an error for default, which is ignore",
+    test_extra_default_config_dict,
+    pydantic_env(),
+    r#"
+from pydantic import BaseModel, ConfigDict
+
+class ModelForbid(BaseModel):
+    ConfigDict()
+    x: int
+
+ModelForbid(x=1, y=2) # E: Unexpected keyword argument `y` in function `ModelForbid.__init__`
+"#,
+);
+
+// Note, Zeina: For ConfigDict, we extra can take any value and it won't raise an error
+// the list of valid keywords is still 'allow', 'forbid' and 'ignore' (as well as None).
+// Values like True/False seem legitimate and won't cause an error but won't actually apply to the model
+// The errors here are raised directly from the stub definitions
+// I think the unexpected keyword error is less relevant since the program already won't typecheck statically
+// unless the correct keyword is provided, but we can choose to remove it
+testcase!(
+    test_extra_wrong_type_config_dict,
+    pydantic_env(),
+    r#"
+from pydantic import BaseModel, ConfigDict
+
+class ModelForbid(BaseModel):
+    ConfigDict(extra=False) # E: Argument `Literal[False]` is not assignable to parameter `extra` with type `Literal['allow', 'forbid', 'ignore'] | None`
+    x: int
+
+ModelForbid(x=1, y=2) # E: Unexpected keyword argument `y` in function `ModelForbid.__init__`
+"#,
+);
+
+testcase!(
+    test_extra_wrong_literal_config_dict,
+    pydantic_env(),
+    r#"
+from pydantic import BaseModel, ConfigDict
+
+class ModelForbid(BaseModel):
+    ConfigDict(extra="123") # E: Argument `Literal['123']` is not assignable to parameter `extra` with type `Literal['allow', 'forbid', 'ignore'] | None`
+    x: int
+
+ModelForbid(x=1, y=2) # E: Unexpected keyword argument `y` in function `ModelForbid.__init__`
+"#,
+);
