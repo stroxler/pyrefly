565a6622777d306a086481c176387b09d82fbde8
Zeina Migeed <migeedz@meta.com>
2025-08-26 15:45:11 -0700

    Add failing alias test that shows a bug in requiredness of parameter
    Reviewed By: rchen152
    
    Differential Revision: D81071525
    
    fbshipit-source-id: 4847eb5bc919512dc6c6bde56ce6214eb1aa273a

--- pyrefly/lib/test/pydantic/aliases.rs
+++ pyrefly/lib/test/pydantic/aliases.rs
@@ -93,3 +93,15 @@ Model(x=0)
 Model(y=0) # E: Missing argument `x` in function `Model.__init__` # E: Unexpected keyword argument `y` in function `Model.__init__`
 "#,
 );
+
+testcase!(
+    bug = "when both validation keys are true, aliases should be required. mypy doesn't support the right behavior here either. This requires generating overloads. We might shelve this for v2",
+    test_validation_defaults,
+    pydantic_env(),
+    r#"
+from pydantic import BaseModel, Field
+class Model(BaseModel, validate_by_name=True, validate_by_alias=True):
+    x: int = Field(alias='y')
+Model()
+"#,
+);
