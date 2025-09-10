f59dc6d43dccda1566284034fffbbca8f92e5412
Steven Troxler <stroxler@meta.com>
2025-09-10 12:05:29 -0700

    Fix clippy lint error in `query.rs`
    Reviewed By: rchen152
    
    Differential Revision: D82119380
    
    fbshipit-source-id: bd3b02654bf972cd5e85eafd1e6360be1081a46a

--- pyrefly/lib/query.rs
+++ pyrefly/lib/query.rs
@@ -514,7 +514,7 @@ impl Query {
                 let def0 = &defs[0];
                 if def0.module.name() == handle.module() {
                     match &def0.metadata {
-                        DefinitionMetadata::Variable(s) => {
+                        DefinitionMetadata::Variable(..) => {
                             let name = module_info.code_at(defs[0].definition_range);
                             vec![Callee {
                                 kind: String::from(CALLEE_KIND_FUNCTION),
@@ -691,7 +691,7 @@ impl Query {
                         class_name,
                     }]
                 }
-                Type::Callable(c) => for_callable(callee_range, module_info, transaction, handle),
+                Type::Callable(..) => for_callable(callee_range, module_info, transaction, handle),
                 Type::Type(box ty) => {
                     init_or_new_from_type(ty, callee_range, transaction, handle, module_info)
                 }
