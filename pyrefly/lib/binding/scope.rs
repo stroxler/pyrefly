79db51bc97d1a42db88b7e9939543239184f8a6c
Steven Troxler <stroxler@meta.com>
2025-09-12 16:10:42 -0700

    Add missing doc comment
    Summary:
    It's self-explanatory what `style` is, but it's nice to note how
    it's used because otherwise you have to dig around to find out.
    
    Reviewed By: rchen152
    
    Differential Revision: D82325998
    
    fbshipit-source-id: fdc587a3628084efca612fc7d3511b703e475138

--- pyrefly/lib/binding/scope.rs
+++ pyrefly/lib/binding/scope.rs
@@ -71,6 +71,7 @@ pub struct StaticInfo {
     pub annot: Option<Idx<KeyAnnotation>>,
     /// How many times this will be redefined
     pub count: usize,
+    /// How was this defined? Needed to determine the key for forward lookups.
     pub style: DefinitionStyle,
     /// Is this a mutable catpure of a definition in an outer scope (i.e. a global or a nonlocal)?
     pub is_mutable_capture: bool,
