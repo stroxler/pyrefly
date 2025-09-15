dc76cadfb5dc120b3bb3c17e058831c8034b4c8a
Conner Nilsen <connernilsen@meta.com>
2025-09-15 13:14:44 -0700

    Refactor BXL query structs into query.rs
    Summary: This diff just handles moving a lot of the BXL query structs into query.rs, since I'm going to convert it to a slightly different structure later to keep future processing simple.
    
    Reviewed By: grievejia
    
    Differential Revision: D81627651
    
    fbshipit-source-id: fdab5c24a71d93fb18335da1e857f94c3cd41122

--- crates/pyrefly_build/src/buck/bxl.rs
+++ crates/pyrefly_build/src/buck/bxl.rs
@@ -5,101 +5,17 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-use std::ops::Deref;
 use std::path::Path;
 use std::path::PathBuf;
 
-use dupe::Dupe as _;
 use pyrefly_python::module_name::ModuleName;
 use pyrefly_python::module_path::ModulePath;
-use pyrefly_python::sys_info::PythonPlatform;
-use pyrefly_python::sys_info::PythonVersion;
 use pyrefly_python::sys_info::SysInfo;
 use serde::Deserialize;
-use starlark_map::small_map::SmallMap;
-use starlark_map::small_set::SmallSet;
-use vec1::Vec1;
 
+use crate::buck::query::TargetManifestDatabase;
 use crate::handle::Handle;
 use crate::source_db::SourceDatabase;
-use crate::source_db::Target;
-
-#[derive(Debug, PartialEq, Eq, Deserialize)]
-#[serde(untagged)]
-pub(crate) enum TargetManifest {
-    Library {
-        deps: SmallSet<Target>,
-        srcs: SmallMap<ModuleName, Vec1<PathBuf>>,
-        python_version: PythonVersion,
-        python_platform: PythonPlatform,
-    },
-    Alias {
-        alias: Target,
-    },
-}
-
-impl TargetManifest {
-    #[expect(unused)]
-    pub fn new_library(
-        srcs: SmallMap<ModuleName, Vec1<PathBuf>>,
-        deps: SmallSet<Target>,
-        sys_info: SysInfo,
-    ) -> Self {
-        Self::Library {
-            srcs,
-            deps,
-            python_version: sys_info.version().dupe(),
-            python_platform: sys_info.platform().clone(),
-        }
-    }
-
-    #[expect(unused)]
-    pub fn new_alias(alias: Target) -> Self {
-        Self::Alias { alias }
-    }
-
-    pub(crate) fn iter_srcs<'a>(
-        &'a self,
-    ) -> Box<dyn Iterator<Item = (&'a ModuleName, &'a Vec1<PathBuf>)> + 'a> {
-        match &self {
-            Self::Library { srcs, .. } => Box::new(srcs.iter()),
-            Self::Alias { .. } => Box::new(std::iter::empty::<(&ModuleName, &Vec1<PathBuf>)>()),
-        }
-    }
-}
-
-#[derive(Debug, PartialEq, Eq, Deserialize)]
-pub(crate) struct TargetManifestDatabase {
-    db: SmallMap<Target, TargetManifest>,
-    root: PathBuf,
-}
-
-impl Deref for TargetManifestDatabase {
-    type Target = SmallMap<Target, TargetManifest>;
-
-    fn deref(&self) -> &Self::Target {
-        &self.db
-    }
-}
-
-impl TargetManifestDatabase {
-    #[expect(unused)]
-    pub fn new(db: SmallMap<Target, TargetManifest>, root: PathBuf) -> Self {
-        Self { db, root }
-    }
-
-    #[allow(unused)]
-    pub(crate) fn iter_srcs<'a>(
-        &'a self,
-    ) -> impl Iterator<Item = (&'a Target, &'a ModuleName, Vec1<PathBuf>)> + 'a {
-        let root = &self.root;
-        self.db.iter().flat_map(move |(target, manifest)| {
-            manifest
-                .iter_srcs()
-                .map(move |(module, paths)| (target, module, paths.mapped_ref(|p| root.join(p))))
-        })
-    }
-}
 
 #[derive(Debug, PartialEq, Eq, Deserialize)]
 pub struct BuckSourceDatabase {
--- crates/pyrefly_build/src/buck/query.rs
+++ crates/pyrefly_build/src/buck/query.rs
@@ -6,12 +6,25 @@
  */
 
 use std::ffi::OsStr;
+use std::ops::Deref;
 use std::path::Path;
 use std::path::PathBuf;
 use std::process::Command;
 
+use dupe::Dupe as _;
+use pyrefly_python::module_name::ModuleName;
+use pyrefly_python::sys_info::PythonPlatform;
+use pyrefly_python::sys_info::PythonVersion;
+use pyrefly_python::sys_info::SysInfo;
+use serde::Deserialize;
+use starlark_map::small_map::SmallMap;
+use starlark_map::small_set::SmallSet;
+use vec1::Vec1;
+
+use crate::source_db::Target;
+
 #[expect(unused)]
-fn query_source_db<'a>(
+pub fn query_source_db<'a>(
     files: impl Iterator<Item = &'a PathBuf>,
     cwd: &Path,
 ) -> anyhow::Result<Vec<u8>> {
@@ -37,4 +50,81 @@ fn query_source_db<'a>(
     }
 
     Ok(result.stdout)
+}
+
+#[derive(Debug, PartialEq, Eq, Deserialize)]
+#[serde(untagged)]
+pub(crate) enum TargetManifest {
+    Library {
+        deps: SmallSet<Target>,
+        srcs: SmallMap<ModuleName, Vec1<PathBuf>>,
+        python_version: PythonVersion,
+        python_platform: PythonPlatform,
+    },
+    Alias {
+        alias: Target,
+    },
+}
+
+impl TargetManifest {
+    #[expect(unused)]
+    pub fn new_library(
+        srcs: SmallMap<ModuleName, Vec1<PathBuf>>,
+        deps: SmallSet<Target>,
+        sys_info: SysInfo,
+    ) -> Self {
+        Self::Library {
+            srcs,
+            deps,
+            python_version: sys_info.version().dupe(),
+            python_platform: sys_info.platform().clone(),
+        }
+    }
+
+    #[expect(unused)]
+    pub fn new_alias(alias: Target) -> Self {
+        Self::Alias { alias }
+    }
+
+    pub(crate) fn iter_srcs<'a>(
+        &'a self,
+    ) -> Box<dyn Iterator<Item = (&'a ModuleName, &'a Vec1<PathBuf>)> + 'a> {
+        match &self {
+            Self::Library { srcs, .. } => Box::new(srcs.iter()),
+            Self::Alias { .. } => Box::new(std::iter::empty::<(&ModuleName, &Vec1<PathBuf>)>()),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Deserialize)]
+pub(crate) struct TargetManifestDatabase {
+    db: SmallMap<Target, TargetManifest>,
+    root: PathBuf,
+}
+
+impl Deref for TargetManifestDatabase {
+    type Target = SmallMap<Target, TargetManifest>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.db
+    }
+}
+
+impl TargetManifestDatabase {
+    #[expect(unused)]
+    pub fn new(db: SmallMap<Target, TargetManifest>, root: PathBuf) -> Self {
+        Self { db, root }
+    }
+
+    #[allow(unused)]
+    pub(crate) fn iter_srcs<'a>(
+        &'a self,
+    ) -> impl Iterator<Item = (&'a Target, &'a ModuleName, Vec1<PathBuf>)> + 'a {
+        let root = &self.root;
+        self.db.iter().flat_map(move |(target, manifest)| {
+            manifest
+                .iter_srcs()
+                .map(move |(module, paths)| (target, module, paths.mapped_ref(|p| root.join(p))))
+        })
+    }
 }
