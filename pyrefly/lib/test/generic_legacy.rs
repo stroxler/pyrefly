01bb96fa1e7ce0c77466ff2af614493059599c52
Steven Troxler <stroxler@meta.com>
2025-09-24 14:53:34 -0700

    Track a bug in module-atter legacy tparam handling
    Summary:
    The test case describes the bug pretty clearly. This might actually cause some major
    confusion for users, it's probably somewhat important to address in the moderately
    near future.
    
    This test is also useful as a regression test of existing code - an earlier version of
    D83156252 had a panic that wasn't caught by our test suite, but is with this test in the mix
    
    Reviewed By: rchen152
    
    Differential Revision: D83177891
    
    fbshipit-source-id: 976efffe99fcb0dc68d95a7f577aa3e046e5d6f8

--- pyrefly/lib/test/generic_legacy.rs
+++ pyrefly/lib/test/generic_legacy.rs
@@ -730,3 +730,33 @@ class A:
     pass
     "#,
 );
+
+testcase!(
+    bug = "We currently create separate narrows for modules that may contain legacy type variables, we need to merge them",
+    test_mutliple_possible_legacy_tparams,
+    TestEnv::one(
+        "foo",
+        "from typing import TypeVar\nT = TypeVar('T')\nclass C: pass"
+    ),
+    r#"
+from typing import Generic, assert_type
+import foo
+
+# Here, the `foo.C` possible-legacy-tparam binding is the one that winds up in scope, we
+# lose track of the `foo.T` one. It probably doesn't matter very much since we at least
+# understand the signature correctly.
+def f(x: foo.T, y: foo.C) -> foo.T:
+    z: foo.T = x  # E: `T` is not assignable to `TypeVar[T]`
+    return z  # E: Returned type `TypeVar[T]` is not assignable to declared return type `T
+assert_type(f(1, foo.C()), int)
+
+# The same thing happens here, but it's a much bigger problem because now we forget
+# about the type variable identity for the entire class body, so the signatures come out
+# wrong.
+class MyList(Generic[foo.T], list[tuple[foo.C, foo.T]]):
+    def my_append(self, c: foo.C, t: foo.T):
+        self.append((c, t))  # E: Argument `tuple[C, TypeVar[T]]` is not assignable to parameter `object` with type `tuple[C, T]` in function `list.append`
+my_list: MyList[int] = MyList()
+my_list.my_append(foo.C(), 5)  # E: Argument `Literal[5]` is not assignable to parameter `t` with type `TypeVar[T]` in function `MyList.my_append`
+    "#,
+);
