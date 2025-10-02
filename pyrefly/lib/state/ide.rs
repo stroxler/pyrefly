fbbcb6c6e938baba0df34a5fb1afd0f9d4563b6b
Danny Yang <yangdanny@meta.com>
2025-10-02 11:58:41 -0700

    insert import after docstring comments
    Summary:
    Instead of inserting at the first statement, insert at the first non-comment statement
    
    fixes https://github.com/facebook/pyrefly/issues/1022
    
    Reviewed By: connernilsen
    
    Differential Revision: D83761243
    
    fbshipit-source-id: 557092bab457eae6d33b97c241697ed32f25ec63

--- pyrefly/lib/state/ide.rs
+++ pyrefly/lib/state/ide.rs
@@ -14,6 +14,7 @@ use pyrefly_python::symbol_kind::SymbolKind;
 use pyrefly_util::gas::Gas;
 use ruff_python_ast::Expr;
 use ruff_python_ast::ModModule;
+use ruff_python_ast::helpers::is_docstring_stmt;
 use ruff_python_ast::name::Name;
 use ruff_text_size::Ranged;
 use ruff_text_size::TextRange;
@@ -206,7 +207,7 @@ pub fn insert_import_edit_with_forced_import_format(
     export_name: &str,
     use_absolute_import: bool,
 ) -> (TextSize, String) {
-    let position = if let Some(first_stmt) = ast.body.first() {
+    let position = if let Some(first_stmt) = ast.body.iter().find(|stmt| !is_docstring_stmt(stmt)) {
         first_stmt.range().start()
     } else {
         ast.range.end()
