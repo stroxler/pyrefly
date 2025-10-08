5c9473130324655679cc93f87121c225c11e3cbc
Danny Yang <yangdanny@meta.com>
2025-10-08 09:02:01 -0700

    update hover tests to be new style LSP tests
    Summary: i copied these for the provide-types tests, and since those are migrated to the new style i might as well migrate these too.
    
    Reviewed By: stroxler
    
    Differential Revision: D84106104
    
    fbshipit-source-id: 182bddbeae3cea3c5563fa1bec82b0865378ae7a

--- pyrefly/lib/test/lsp/lsp_interaction/hover.rs
+++ pyrefly/lib/test/lsp/lsp_interaction/hover.rs
@@ -5,88 +5,65 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-use lsp_server::Message;
-use lsp_server::Request;
-use lsp_server::RequestId;
 use lsp_server::Response;
 use lsp_types::Url;
 
-use crate::test::lsp::lsp_interaction::util::TestCase;
-use crate::test::lsp::lsp_interaction::util::build_did_open_notification;
+use crate::test::lsp::lsp_interaction::object_model::InitializeSettings;
+use crate::test::lsp::lsp_interaction::object_model::LspInteraction;
 use crate::test::lsp::lsp_interaction::util::get_test_files_root;
-#[allow(deprecated)]
-use crate::test::lsp::lsp_interaction::util::run_test_lsp;
 
 #[test]
-#[allow(deprecated)]
 fn test_hover_basic() {
     let root = get_test_files_root();
-    let request_file_name = root.path().join("bar.py");
-    run_test_lsp(TestCase {
-        messages_from_language_client: vec![
-            Message::from(build_did_open_notification(
-                root.path().join(request_file_name.clone()),
-            )),
-            Message::from(Request {
-                id: RequestId::from(2),
-                method: "textDocument/hover".to_owned(),
-                params: serde_json::json!({
-                    "textDocument": {
-                        "uri": Url::from_file_path(root.path().join(request_file_name)).unwrap().to_string()
-                    },
-                    "position": {
-                        "line": 7,
-                        "character": 5
-                    }
-                }),
-            }),
-        ],
-        expected_messages_from_language_server: vec![Message::Response(Response {
-            id: RequestId::from(2),
-            result: Some(serde_json::json!({
-                "contents": {
-                    "kind": "markdown",
-                    "value": "```python\n(variable) foo: Literal[3]\n```",
-                }
-            })),
-            error: None,
-        })],
+    let mut interaction = LspInteraction::new();
+    interaction.set_root(root.path().to_path_buf());
+    interaction.initialize(InitializeSettings {
+        configuration: Some(None),
         ..Default::default()
     });
+
+    interaction.server.did_open("bar.py");
+    interaction.server.hover("bar.py", 7, 5);
+
+    interaction.client.expect_response(Response {
+        id: interaction.server.current_request_id(),
+        result: Some(serde_json::json!({
+            "contents": {
+                "kind": "markdown",
+                "value": "```python\n(variable) foo: Literal[3]\n```",
+            }
+        })),
+        error: None,
+    });
+
+    interaction.shutdown();
 }
 
 #[test]
-#[allow(deprecated)]
 fn test_hover() {
     let root = get_test_files_root();
+    let mut interaction = LspInteraction::new();
+    interaction.set_root(root.path().to_path_buf());
+    interaction.initialize(InitializeSettings {
+        configuration: Some(None),
+        ..Default::default()
+    });
 
-    run_test_lsp(TestCase {
-        messages_from_language_client: vec![
-            Message::from(build_did_open_notification(root.path().join("foo.py"))),
-            Message::from(Request {
-                id: RequestId::from(2),
-                method: "textDocument/hover".to_owned(),
-                params: serde_json::json!({
-                    "textDocument": {
-                        "uri": Url::from_file_path(root.path().join("foo.py")).unwrap().to_string()
-                    },
-                    "position": {
-                        "line": 6,
-                        "character": 16
-                    }
-                }),
-            }),
-        ],
-        expected_messages_from_language_server: vec![Message::Response(Response {
-            id: RequestId::from(2),
-            result: Some(serde_json::json!({"contents": {
+    interaction.server.did_open("foo.py");
+    interaction.server.hover("foo.py", 6, 16);
+
+    interaction.client.expect_response(Response {
+        id: interaction.server.current_request_id(),
+        result: Some(serde_json::json!({
+            "contents": {
                 "kind": "markdown",
                 "value": "```python\n(class) Bar: type[Bar]\n```\n\nGo to [Bar](".to_owned()
                     + Url::from_file_path(root.path().join("bar.py")).unwrap().as_str()
                     + "#L7,7)",
-            }})),
-            error: None,
-        })],
-        ..Default::default()
+            }
+        })),
+        error: None,
     });
+
+    interaction.shutdown();
 }
