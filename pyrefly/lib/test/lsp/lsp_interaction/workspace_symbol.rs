f511877967d424711cdac14b3abbdfdc850a6366
John Van Schultz <jvansch@meta.com>
2025-10-10 09:42:29 -0700

    Update lsp_interaction/workspace_symbol.rs to use the object model rather than the legacy functional model
    Summary: Update workplace_symbol.rs to use lsp_interaction::object_model
    
    Reviewed By: stroxler
    
    Differential Revision: D84355125
    
    fbshipit-source-id: 615eb1e885d50cdc21f66f130558d097c0b449ba

--- pyrefly/lib/test/lsp/lsp_interaction/workspace_symbol.rs
+++ pyrefly/lib/test/lsp/lsp_interaction/workspace_symbol.rs
@@ -11,47 +11,52 @@ use lsp_server::RequestId;
 use lsp_server::Response;
 use lsp_types::Url;
 
-use crate::commands::lsp::IndexingMode;
-use crate::test::lsp::lsp_interaction::util::TestCase;
-use crate::test::lsp::lsp_interaction::util::build_did_open_notification;
+use crate::test::lsp::lsp_interaction::object_model::InitializeSettings;
+use crate::test::lsp::lsp_interaction::object_model::LspInteraction;
 use crate::test::lsp::lsp_interaction::util::get_test_files_root;
-#[allow(deprecated)]
-use crate::test::lsp::lsp_interaction::util::run_test_lsp;
 
 #[test]
-#[allow(deprecated)]
 fn test_workspace_symbol() {
     let root = get_test_files_root();
     let root_path = root.path().join("tests_requiring_config");
     let scope_uri = Url::from_file_path(root_path.clone()).unwrap();
-
-    run_test_lsp(TestCase {
-        messages_from_language_client: vec![
-            Message::from(build_did_open_notification(root_path.join("foo.py"))),
-            Message::from(Request {
-                id: RequestId::from(2),
-                method: "workspace/symbol".to_owned(),
-                params: serde_json::json!({
-                    "query": "this_is_a_very_long_function_name_so_we_can"
-                }),
-            }),
-        ],
-        expected_messages_from_language_server: vec![Message::Response(Response {
-            id: RequestId::from(2),
-            result: Some(serde_json::json!([
-                {
-                    "kind":12,
-                    "location":{
-                        "range":{"start":{"line":6,"character":4},"end":{"line":6,"character":99}},
-                        "uri":Url::from_file_path(root_path.join("autoimport_provider.py")).unwrap().to_string()
-                    },
-                    "name":"this_is_a_very_long_function_name_so_we_can_deterministically_test_autoimport_with_fuzzy_search"
-                }
-            ])),
-            error: None,
-        })],
-        indexing_mode: IndexingMode::LazyBlocking,
+    let mut interaction = LspInteraction::new();
+    interaction.set_root(root_path.clone());
+    interaction.initialize(InitializeSettings {
         workspace_folders: Some(vec![("test".to_owned(), scope_uri)]),
+        configuration: Some(Some(
+            serde_json::json!([{ "indexing_mode": "lazy_blocking"}]),
+        )),
         ..Default::default()
     });
+
+    interaction.server.did_open("autoimport_provider.py");
+
+    interaction.server.send_message(Message::Request(Request {
+        id: RequestId::from(2),
+        method: "workspace/symbol".to_owned(),
+        params: serde_json::json!({
+            "query": "this_is_a_very_long_function_name_so_we_can"
+        }),
+    }));
+
+    interaction.client.expect_response(Response {
+        id: RequestId::from(2),
+        result: Some(serde_json::json!([
+            {
+                "kind": 12,
+                "location": {
+                    "range": {
+                        "start": {"line": 6, "character": 4},
+                        "end": {"line": 6, "character": 99}
+                    },
+                    "uri": Url::from_file_path(root_path.join("autoimport_provider.py")).unwrap().to_string()
+                },
+                "name": "this_is_a_very_long_function_name_so_we_can_deterministically_test_autoimport_with_fuzzy_search"
+            }
+        ])),
+        error: None,
+    });
+
+    interaction.shutdown();
 }
