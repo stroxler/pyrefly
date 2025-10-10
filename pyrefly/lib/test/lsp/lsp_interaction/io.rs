e4025f4605d939bb96c9ea2d08bf85a4b13ee481
John Van Schultz <jvansch@meta.com>
2025-10-10 09:26:24 -0700

    Update lsp_interaction/io.rs to use the object model rather than legacy functional model.
    Summary: Updates all tests in io.rs to use lsp_interaction::object_model
    
    Reviewed By: stroxler
    
    Differential Revision: D84351909
    
    fbshipit-source-id: d6900fc23d46195bf9c2645bfcc464c1d030a2b4

--- pyrefly/lib/test/lsp/lsp_interaction/io.rs
+++ pyrefly/lib/test/lsp/lsp_interaction/io.rs
@@ -12,113 +12,97 @@ use lsp_server::RequestId;
 use lsp_server::Response;
 use lsp_types::Url;
 
-use crate::test::lsp::lsp_interaction::util::TestCase;
-use crate::test::lsp::lsp_interaction::util::build_did_open_notification;
+use crate::test::lsp::lsp_interaction::object_model::InitializeSettings;
+use crate::test::lsp::lsp_interaction::object_model::LspInteraction;
 use crate::test::lsp::lsp_interaction::util::get_test_files_root;
-#[allow(deprecated)]
-use crate::test::lsp::lsp_interaction::util::run_test_lsp;
 
 #[test]
-#[allow(deprecated)]
 fn test_edits_while_recheck() {
-    let mut test_messages = Vec::new();
-    let mut expected_responses = Vec::new();
     let root = get_test_files_root();
+    let mut interaction = LspInteraction::new();
+    interaction.set_root(root.path().to_path_buf());
+    interaction.initialize(InitializeSettings::default());
 
+    interaction.server.did_open("foo.py");
     let path = root.path().join("foo.py");
-    test_messages.push(Message::from(build_did_open_notification(path.clone())));
     // In this test, we trigger didSave and didChange to try to exercise the behavior
     // where we have concurrent in-memory recheck and on-disk recheck.
-    test_messages.push(Message::from(Notification {
-        method: "textDocument/didSave".to_owned(),
-        params: serde_json::json!({
-            "textDocument": {
-                "uri": Url::from_file_path(&path).unwrap().to_string(),
-                "languageId": "python",
-                "version": 1,
-                "text": std::fs::read_to_string(path.clone()).unwrap()
-            }
-        }),
-    }));
-    test_messages.push(Message::from(Notification {
-        method: "textDocument/didChange".to_owned(),
-        params: serde_json::json!({
-            "textDocument": {
-                "uri": Url::from_file_path(&path).unwrap().to_string(),
-                "languageId": "python",
-                "version": 2
-            },
-            "contentChanges": [
-                {"text": format!("{}\n\nextra_stuff", std::fs::read_to_string(path).unwrap())}
-            ],
-        }),
-    }));
-
-    test_messages.push(Message::from(Request {
-        id: RequestId::from(2),
-        method: "textDocument/definition".to_owned(),
-        params: serde_json::json!({
-            "textDocument": {
-                "uri": Url::from_file_path(root.path().join("foo.py")).unwrap().to_string()
-            },
-            "position": {
-                "line": 6,
-                "character": 18
-            }
-        }),
-    }));
-
-    expected_responses.push(Message::Response(Response {
-        id: RequestId::from(2),
-        result: Some(serde_json::json!({
-            "uri": Url::from_file_path(root.path().join("bar.py")).unwrap().to_string(),
-            "range": {
-                "start": {
-                    "line": 6,
-                    "character": 6
-                },
-                "end": {
-                    "line": 6,
-                    "character": 9
+    interaction
+        .server
+        .send_message(Message::Notification(Notification {
+            method: "textDocument/didSave".to_owned(),
+            params: serde_json::json!({
+                "textDocument": {
+                    "uri": Url::from_file_path(&path).unwrap().to_string(),
+                    "languageId": "python",
+                    "version": 1,
+                    "text": std::fs::read_to_string(path.clone()).unwrap()
                 }
-            }
-        })),
-        error: None,
-    }));
+            }),
+        }));
 
-    run_test_lsp(TestCase {
-        messages_from_language_client: test_messages,
-        expected_messages_from_language_server: expected_responses,
-        ..Default::default()
-    });
+    interaction
+        .server
+        .send_message(Message::Notification(Notification {
+            method: "textDocument/didChange".to_owned(),
+            params: serde_json::json!({
+                "textDocument": {
+                    "uri": Url::from_file_path(&path).unwrap().to_string(),
+                    "languageId": "python",
+                    "version": 2
+                },
+                "contentChanges": [
+                    {"text": format!("{}\n\nextra_stuff", std::fs::read_to_string(&path).unwrap())}
+                ],
+            }),
+        }));
+
+    interaction.server.definition("foo.py", 6, 18);
+
+    interaction
+        .client
+        .expect_definition_response_from_root("bar.py", 6, 6, 6, 9);
+
+    interaction.shutdown();
 }
 
 #[test]
-#[allow(deprecated)]
 fn test_file_watcher() {
     let root = get_test_files_root();
-    run_test_lsp(TestCase {
-        messages_from_language_client: vec![Message::Response(Response {
-            id: RequestId::from(1),
-            result: None,
-            error: None,
-        })],
-        expected_messages_from_language_server: vec![Message::Request(Request {
-            id: RequestId::from(1),
-            method: "client/registerCapability".to_owned(),
-            params: serde_json::json!({
-            "registrations": [{"id": "FILEWATCHER", "method": "workspace/didChangeWatchedFiles", "registerOptions": {"watchers": [
-                {"globPattern": root.path().join("**/*.py").to_str().unwrap(), "kind": 7},
-                {"globPattern": root.path().join("**/*.pyi").to_str().unwrap(), "kind": 7},
-                {"globPattern": root.path().join("**/pyrefly.toml"), "kind": 7},
-                {"globPattern": root.path().join("**/pyproject.toml"), "kind": 7}
-            ]}}]}),
-        })],
-        workspace_folders: Some(vec![(
-            "test".to_owned(),
-            Url::from_file_path(root).unwrap(),
-        )]),
+    let mut interaction = LspInteraction::new();
+    interaction.set_root(root.path().to_path_buf());
+
+    let scope_uri = Url::from_file_path(root.path()).unwrap();
+    interaction.initialize(InitializeSettings {
+        workspace_folders: Some(vec![("test".to_owned(), scope_uri.clone())]),
         file_watch: true,
         ..Default::default()
     });
+
+    interaction.client.expect_request(Request {
+        id: RequestId::from(1),
+        method: "client/registerCapability".to_owned(),
+        params: serde_json::json!({
+            "registrations": [{
+                "id": "FILEWATCHER",
+                "method": "workspace/didChangeWatchedFiles",
+                "registerOptions": {
+                    "watchers": [
+                        {"globPattern": root.path().join("**/*.py").to_str().unwrap(), "kind": 7},
+                        {"globPattern": root.path().join("**/*.pyi").to_str().unwrap(), "kind": 7},
+                        {"globPattern": root.path().join("**/pyrefly.toml"), "kind": 7},
+                        {"globPattern": root.path().join("**/pyproject.toml"), "kind": 7}
+                    ]
+                }
+            }]
+        }),
+    });
+
+    interaction.server.send_message(Message::Response(Response {
+        id: RequestId::from(1),
+        result: None,
+        error: None,
+    }));
+
+    interaction.shutdown();
 }
