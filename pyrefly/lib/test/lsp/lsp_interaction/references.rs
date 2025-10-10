31f9efa43ec21dbb188c95f54f3edb41653537bd
John Van Schultz <jvansch@meta.com>
2025-10-10 09:44:17 -0700

    Update lsp_interaction/references.rs to use the object_model rather than the legacy functional model
    Summary: Updates references.rs to use lsp_interaction::object_model
    
    Reviewed By: stroxler
    
    Differential Revision: D84357483
    
    fbshipit-source-id: 8118109293b45fe599d5954be75cda2d2af60c22

--- pyrefly/lib/test/lsp/lsp_interaction/references.rs
+++ pyrefly/lib/test/lsp/lsp_interaction/references.rs
@@ -7,53 +7,66 @@
 
 use lsp_server::Message;
 use lsp_server::Notification;
-use lsp_server::Request;
 use lsp_server::RequestId;
 use lsp_server::Response;
 use lsp_types::Url;
 
-use crate::commands::lsp::IndexingMode;
-use crate::test::lsp::lsp_interaction::util::TestCase;
-use crate::test::lsp::lsp_interaction::util::build_did_open_notification;
+use crate::test::lsp::lsp_interaction::object_model::InitializeSettings;
+use crate::test::lsp::lsp_interaction::object_model::LspInteraction;
 use crate::test::lsp::lsp_interaction::util::get_test_files_root;
-#[allow(deprecated)]
-use crate::test::lsp::lsp_interaction::util::run_test_lsp;
 
 #[test]
-#[allow(deprecated)]
 fn test_references() {
     let root = get_test_files_root();
     let root_path = root.path().join("tests_requiring_config");
     let scope_uri = Url::from_file_path(root_path.clone()).unwrap();
-    let mut test_messages = Vec::new();
-    let mut expected_responses = Vec::new();
+    let mut interaction = LspInteraction::new();
+    interaction.set_root(root_path.clone());
+    interaction.initialize(InitializeSettings {
+        workspace_folders: Some(vec![("test".to_owned(), scope_uri)]),
+        configuration: Some(None),
+        ..Default::default()
+    });
+
     let foo = root_path.join("foo.py");
     let bar = root_path.join("bar.py");
     let various_imports = root_path.join("various_imports.py");
     let with_synthetic_bindings = root_path.join("with_synthetic_bindings.py");
-    test_messages.push(Message::from(build_did_open_notification(bar.clone())));
 
-    // Find reference from a reference location in the same in-memory file
-    test_messages.push(Message::from(Request {
-        id: RequestId::from(2),
-        method: "textDocument/references".to_owned(),
-        params: serde_json::json!({
-            "textDocument": {
-                "uri": Url::from_file_path(bar.clone()).unwrap().to_string()
-            },
-            "position": {
-                "line": 10,
-                "character": 1
-            },
-            "context": {
-                "includeDeclaration": true
-            },
-        }),
-    }));
+    interaction.server.did_open("foo.py");
+    interaction.server.did_open("various_imports.py");
+    interaction.server.did_open("with_synthetic_bindings.py");
+    interaction.server.did_open("bar.py");
+    interaction
+        .server
+        .send_message(Message::Request(lsp_server::Request {
+            id: RequestId::from(2),
+            method: "textDocument/references".to_owned(),
+            params: serde_json::json!({
+                "textDocument": {
+                    "uri": Url::from_file_path(bar.clone()).unwrap().to_string()
+                },
+                "position": {
+                    "line": 10,
+                    "character": 1
+                },
+                "context": {
+                    "includeDeclaration": true
+                },
+            }),
+        }));
 
-    expected_responses.push(Message::Response(Response {
+    interaction.client.expect_response(Response {
         id: RequestId::from(2),
         result: Some(serde_json::json!([
+            {
+                "range": {"start":{"line":6,"character":6},"end":{"character":9,"line":6}},
+                "uri": Url::from_file_path(bar.clone()).unwrap().to_string()
+            },
+            {
+                "range": {"start":{"line":10,"character":0},"end":{"character":3,"line":10}},
+                "uri": Url::from_file_path(bar.clone()).unwrap().to_string()
+            },
             {
                 "range": {"start":{"line":6,"character":16},"end":{"line":6,"character":19}},
                 "uri": Url::from_file_path(foo.clone()).unwrap().to_string()
@@ -82,6 +95,32 @@ fn test_references() {
                 "range": {"start":{"line":10,"character":4},"end":{"character":7,"line":10}},
                 "uri": Url::from_file_path(with_synthetic_bindings.clone()).unwrap().to_string()
             },
+        ])),
+        error: None,
+    });
+
+    interaction
+        .server
+        .send_message(Message::Request(lsp_server::Request {
+            id: RequestId::from(3),
+            method: "textDocument/references".to_owned(),
+            params: serde_json::json!({
+                "textDocument": {
+                    "uri": Url::from_file_path(bar.clone()).unwrap().to_string()
+                },
+                "position": {
+                    "line": 6,
+                    "character": 7
+                },
+                "context": {
+                    "includeDeclaration": true
+                },
+            }),
+        }));
+
+    interaction.client.expect_response(Response {
+        id: RequestId::from(3),
+        result: Some(serde_json::json!([
             {
                 "range": {"start":{"line":6,"character":6},"end":{"character":9,"line":6}},
                 "uri": Url::from_file_path(bar.clone()).unwrap().to_string()
@@ -90,31 +129,6 @@ fn test_references() {
                 "range": {"start":{"line":10,"character":0},"end":{"character":3,"line":10}},
                 "uri": Url::from_file_path(bar.clone()).unwrap().to_string()
             },
-        ])),
-        error: None,
-    }));
-
-    // Find reference from a definition location in the same in-memory file
-    test_messages.push(Message::from(Request {
-        id: RequestId::from(3),
-        method: "textDocument/references".to_owned(),
-        params: serde_json::json!({
-            "textDocument": {
-                "uri": Url::from_file_path(bar.clone()).unwrap().to_string()
-            },
-            "position": {
-                "line": 6,
-                "character": 7
-            },
-            "context": {
-                "includeDeclaration": true
-            },
-        }),
-    }));
-
-    expected_responses.push(Message::Response(Response {
-        id: RequestId::from(3),
-        result: Some(serde_json::json!([
             {
                 "range": {"start":{"line":6,"character":16},"end":{"line":6, "character":19}},
                 "uri": Url::from_file_path(foo.clone()).unwrap().to_string()
@@ -143,39 +157,30 @@ fn test_references() {
                 "range": {"start":{"line":10,"character":4},"end":{"character":7,"line":10}},
                 "uri": Url::from_file_path(with_synthetic_bindings.clone()).unwrap().to_string()
             },
-            {
-                "range": {"start":{"line":6,"character":6},"end":{"character":9,"line":6}},
-                "uri": Url::from_file_path(bar.clone()).unwrap().to_string()
-            },
-            {
-                "range": {"start":{"line":10,"character":0},"end":{"character":3,"line":10}},
-                "uri": Url::from_file_path(bar.clone()).unwrap().to_string()
-            },
         ])),
         error: None,
-    }));
+    });
 
-    test_messages.push(Message::from(build_did_open_notification(foo.clone())));
+    interaction
+        .server
+        .send_message(Message::Request(lsp_server::Request {
+            id: RequestId::from(4),
+            method: "textDocument/references".to_owned(),
+            params: serde_json::json!({
+                "textDocument": {
+                    "uri": Url::from_file_path(foo.clone()).unwrap().to_string()
+                },
+                "position": {
+                    "line": 6,
+                    "character": 17
+                },
+                "context": {
+                    "includeDeclaration": true
+                },
+            }),
+        }));
 
-    // Find reference from a reference location in a different file
-    test_messages.push(Message::from(Request {
-        id: RequestId::from(4),
-        method: "textDocument/references".to_owned(),
-        params: serde_json::json!({
-            "textDocument": {
-                "uri": Url::from_file_path(foo.clone()).unwrap().to_string()
-            },
-            "position": {
-                "line": 6,
-                "character": 17
-            },
-            "context": {
-                "includeDeclaration": true
-            },
-        }),
-    }));
-
-    expected_responses.push(Message::Response(Response {
+    interaction.client.expect_response(Response {
         id: RequestId::from(4),
         result: Some(serde_json::json!([
             {
@@ -185,6 +190,18 @@ fn test_references() {
             {
                 "range": {"start":{"line":10,"character":0},"end":{"character":3,"line":10}},
                 "uri": Url::from_file_path(bar.clone()).unwrap().to_string()
+            },
+            {
+                "range": {"start":{"line":6,"character":16},"end":{"line":6, "character":19}},
+                "uri": Url::from_file_path(foo.clone()).unwrap().to_string()
+            },
+            {
+                "range": {"start":{"line":8,"character":0},"end":{"line":8,"character":3}},
+                "uri": Url::from_file_path(foo.clone()).unwrap().to_string()
+            },
+            {
+                "range": {"start":{"line":9,"character":4},"end":{"line":9,"character":7}},
+                "uri": Url::from_file_path(foo.clone()).unwrap().to_string()
             },
             {
                 "range": {"start":{"line":5,"character":16},"end":{"line":5,"character":19}},
@@ -202,43 +219,30 @@ fn test_references() {
                 "range": {"start":{"line":10,"character":4},"end":{"character":7,"line":10}},
                 "uri": Url::from_file_path(with_synthetic_bindings.clone()).unwrap().to_string()
             },
-            {
-                "range": {"start":{"line":6,"character":16},"end":{"line":6, "character":19}},
-                "uri": Url::from_file_path(foo.clone()).unwrap().to_string()
-            },
-            {
-                "range": {"start":{"line":8,"character":0},"end":{"line":8,"character":3}},
-                "uri": Url::from_file_path(foo.clone()).unwrap().to_string()
-            },
-            {
-                "range": {"start":{"line":9,"character":4},"end":{"line":9,"character":7}},
-                "uri": Url::from_file_path(foo.clone()).unwrap().to_string()
-            },
         ])),
         error: None,
-    }));
+    });
 
-    test_messages.push(Message::from(build_did_open_notification(
-        various_imports.clone(),
-    )));
-    test_messages.push(Message::from(Request {
-        id: RequestId::from(5),
-        method: "textDocument/references".to_owned(),
-        params: serde_json::json!({
-            "textDocument": {
-                "uri": Url::from_file_path(various_imports.clone()).unwrap().to_string()
-            },
-            "position": {
-                "line": 7,
-                "character": 0
-            },
-            "context": {
-                "includeDeclaration": true
-            },
-        }),
-    }));
+    interaction
+        .server
+        .send_message(Message::Request(lsp_server::Request {
+            id: RequestId::from(5),
+            method: "textDocument/references".to_owned(),
+            params: serde_json::json!({
+                "textDocument": {
+                    "uri": Url::from_file_path(various_imports.clone()).unwrap().to_string()
+                },
+                "position": {
+                    "line": 7,
+                    "character": 0
+                },
+                "context": {
+                    "includeDeclaration": true
+                },
+            }),
+        }));
 
-    expected_responses.push(Message::Response(Response {
+    interaction.client.expect_response(Response {
         id: RequestId::from(5),
         result: Some(serde_json::json!([
             {
@@ -251,40 +255,44 @@ fn test_references() {
             },
         ])),
         error: None,
-    }));
+    });
 
-    // Change the definition file in memory.
-    // However, find ref still reports the stale result based on the filesystem content.
-    test_messages.push(Message::from(Notification {
-        method: "textDocument/didChange".to_owned(),
-        params: serde_json::json!({
-            "textDocument": {
-                "uri": Url::from_file_path(bar.clone()).unwrap().to_string(),
-                "languageId": "python",
-                "version": 2
-            },
-            "contentChanges": [{
-                "text": format!("\n\n{}", std::fs::read_to_string(bar.clone()).unwrap())
-            }],
-        }),
-    }));
-    test_messages.push(Message::from(Request {
-        id: RequestId::from(6),
-        method: "textDocument/references".to_owned(),
-        params: serde_json::json!({
-            "textDocument": {
-                "uri": Url::from_file_path(foo.clone()).unwrap().to_string()
-            },
-            "position": {
-                "line": 6,
-                "character": 17
-            },
-            "context": {
-                "includeDeclaration": true
-            },
-        }),
-    }));
-    expected_responses.push(Message::Response(Response {
+    interaction
+        .server
+        .send_message(Message::Notification(Notification {
+            method: "textDocument/didChange".to_owned(),
+            params: serde_json::json!({
+                "textDocument": {
+                    "uri": Url::from_file_path(bar.clone()).unwrap().to_string(),
+                    "languageId": "python",
+                    "version": 2
+                },
+                "contentChanges": [{
+                    "text": format!("\n\n{}", std::fs::read_to_string(bar.clone()).unwrap())
+                }],
+            }),
+        }));
+
+    interaction
+        .server
+        .send_message(Message::Request(lsp_server::Request {
+            id: RequestId::from(6),
+            method: "textDocument/references".to_owned(),
+            params: serde_json::json!({
+                "textDocument": {
+                    "uri": Url::from_file_path(foo.clone()).unwrap().to_string()
+                },
+                "position": {
+                    "line": 6,
+                    "character": 17
+                },
+                "context": {
+                    "includeDeclaration": true
+                },
+            }),
+        }));
+
+    interaction.client.expect_response(Response {
         id: RequestId::from(6),
         result: Some(serde_json::json!([
             {
@@ -294,14 +302,6 @@ fn test_references() {
             {
                 "range": {"start":{"line":10,"character":0},"end":{"character":3,"line":10}},
                 "uri": Url::from_file_path(bar.clone()).unwrap().to_string()
-            },
-            {
-                "range": {"start":{"line":5,"character":16},"end":{"character":19,"line":5}},
-                "uri": Url::from_file_path(with_synthetic_bindings.clone()).unwrap().to_string()
-            },
-            {
-                "range": {"start":{"line":10,"character":4},"end":{"character":7,"line":10}},
-                "uri": Url::from_file_path(with_synthetic_bindings.clone()).unwrap().to_string()
             },
             {
                 "range": {"start":{"line":6,"character":16},"end":{"line":6, "character":19}},
@@ -323,30 +323,38 @@ fn test_references() {
                 "range": {"start":{"line":5,"character":26},"end":{"line":5,"character":29}},
                 "uri": Url::from_file_path(various_imports.clone()).unwrap().to_string()
             },
+            {
+                "range": {"start":{"line":5,"character":16},"end":{"character":19,"line":5}},
+                "uri": Url::from_file_path(with_synthetic_bindings.clone()).unwrap().to_string()
+            },
+            {
+                "range": {"start":{"line":10,"character":4},"end":{"character":7,"line":10}},
+                "uri": Url::from_file_path(with_synthetic_bindings.clone()).unwrap().to_string()
+            },
         ])),
         error: None,
-    }));
+    });
 
-    // When we do a find-ref in an in-memory file with changed content,
-    // it will cause us to fail to find references in other files.
-    test_messages.push(Message::from(Request {
-        id: RequestId::from(7),
-        method: "textDocument/references".to_owned(),
-        params: serde_json::json!({
-            "textDocument": {
-                "uri": Url::from_file_path(bar.clone()).unwrap().to_string()
-            },
-            "position": {
-                "line": 8,
-                "character": 7
-            },
-            "context": {
-                "includeDeclaration": true
-            },
-        }),
-    }));
+    interaction
+        .server
+        .send_message(Message::Request(lsp_server::Request {
+            id: RequestId::from(7),
+            method: "textDocument/references".to_owned(),
+            params: serde_json::json!({
+                "textDocument": {
+                    "uri": Url::from_file_path(bar.clone()).unwrap().to_string()
+                },
+                "position": {
+                    "line": 8,
+                    "character": 7
+                },
+                "context": {
+                    "includeDeclaration": true
+                },
+            }),
+        }));
 
-    expected_responses.push(Message::Response(Response {
+    interaction.client.expect_response(Response {
         id: RequestId::from(7),
         result: Some(serde_json::json!([
             {
@@ -359,13 +367,7 @@ fn test_references() {
             },
         ])),
         error: None,
-    }));
-
-    run_test_lsp(TestCase {
-        messages_from_language_client: test_messages,
-        expected_messages_from_language_server: expected_responses,
-        indexing_mode: IndexingMode::LazyBlocking,
-        workspace_folders: Some(vec![("test".to_owned(), scope_uri)]),
-        ..Default::default()
     });
+
+    interaction.shutdown();
 }
