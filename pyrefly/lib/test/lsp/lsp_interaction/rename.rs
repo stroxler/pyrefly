481a8bd9f5ab1731394a9c1b4be488749588c08b
John Van Schultz <jvansch@meta.com>
2025-10-10 09:26:04 -0700

    Update lsp_interaction/rename.rs to use the object model rather than legacy functional model.
    Summary: Updates all tests in rename.is to use lsp_interation::object_model
    
    Reviewed By: stroxler
    
    Differential Revision: D84353542
    
    fbshipit-source-id: 541b21b8cb4485467511eba5add53fa28e4b85a8

--- pyrefly/lib/test/lsp/lsp_interaction/rename.rs
+++ pyrefly/lib/test/lsp/lsp_interaction/rename.rs
@@ -11,68 +11,78 @@ use lsp_server::RequestId;
 use lsp_server::Response;
 use lsp_types::Url;
 
-use crate::commands::lsp::IndexingMode;
-use crate::test::lsp::lsp_interaction::util::TestCase;
-use crate::test::lsp::lsp_interaction::util::build_did_open_notification;
+use crate::test::lsp::lsp_interaction::object_model::InitializeSettings;
+use crate::test::lsp::lsp_interaction::object_model::LspInteraction;
 use crate::test::lsp::lsp_interaction::util::get_test_files_root;
-#[allow(deprecated)]
-use crate::test::lsp::lsp_interaction::util::run_test_lsp;
 
 #[test]
-#[allow(deprecated)]
 fn test_prepare_rename() {
     let root = get_test_files_root();
+    let mut interaction = LspInteraction::new();
+    interaction.set_root(root.path().to_path_buf());
+    interaction.initialize(InitializeSettings::default());
 
-    run_test_lsp(TestCase {
-        messages_from_language_client: vec![
-            Message::from(build_did_open_notification(root.path().join("foo.py"))),
-            Message::from(Request {
-                id: RequestId::from(2),
-                method: "textDocument/prepareRename".to_owned(),
-                params: serde_json::json!({
-                    "textDocument": {
-                        "uri": Url::from_file_path(root.path().join("foo.py")).unwrap().to_string()
-                    },
-                    "position": {
-                        "line": 6,
-                        "character": 16
-                    }
-                }),
-            }),
-        ],
-        expected_messages_from_language_server: vec![Message::Response(Response {
-            id: RequestId::from(2),
-            result: Some(serde_json::json!({
-                "start":{"line":6,"character":16},
-                "end":{"line":6,"character":19},
-            })),
-            error: None,
-        })],
-        ..Default::default()
+    interaction.server.did_open("foo.py");
+
+    let path = root.path().join("foo.py");
+    interaction.server.send_message(Message::Request(Request {
+        id: RequestId::from(2),
+        method: "textDocument/prepareRename".to_owned(),
+        params: serde_json::json!({
+            "textDocument": {
+                "uri": Url::from_file_path(&path).unwrap().to_string()
+            },
+            "position": {
+                "line": 6,
+                "character": 16
+            }
+        }),
+    }));
+
+    interaction.client.expect_response(Response {
+        id: RequestId::from(2),
+        result: Some(serde_json::json!({
+            "start": {"line": 6, "character": 16},
+            "end": {"line": 6, "character": 19},
+        })),
+        error: None,
     });
+
+    interaction.shutdown();
 }
 
 #[test]
-#[allow(deprecated)]
 fn test_rename() {
     let root = get_test_files_root();
     let root_path = root.path().join("tests_requiring_config");
     let scope_uri = Url::from_file_path(root_path.clone()).unwrap();
-    let mut test_messages = Vec::new();
-    let mut expected_responses = Vec::new();
+
+    let mut interaction = LspInteraction::new();
+    interaction.set_root(root_path.clone());
+    interaction.initialize(InitializeSettings {
+        workspace_folders: Some(vec![("test".to_owned(), scope_uri.clone())]),
+        configuration: Some(Some(
+            serde_json::json!([{ "indexing_mode": "lazy_blocking" }]),
+        )),
+        ..Default::default()
+    });
+
     let foo = root_path.join("foo.py");
     let bar = root_path.join("bar.py");
     let various_imports = root_path.join("various_imports.py");
     let with_synthetic_bindings = root_path.join("with_synthetic_bindings.py");
-    test_messages.push(Message::from(build_did_open_notification(bar.clone())));
 
-    // Find reference from a reference location in the same in-memory file
-    test_messages.push(Message::from(Request {
+    interaction.server.did_open("bar.py");
+    interaction.server.did_open("foo.py");
+    interaction.server.did_open("various_imports.py");
+    interaction.server.did_open("with_synthetic_bindings.py");
+
+    interaction.server.send_message(Message::Request(Request {
         id: RequestId::from(2),
         method: "textDocument/rename".to_owned(),
         params: serde_json::json!({
             "textDocument": {
-                "uri": Url::from_file_path(bar.clone()).unwrap().to_string()
+                "uri": Url::from_file_path(&bar).unwrap().to_string()
             },
             "position": {
                 "line": 10,
@@ -82,11 +92,11 @@ fn test_rename() {
         }),
     }));
 
-    expected_responses.push(Message::Response(Response {
+    interaction.client.expect_response(Response {
         id: RequestId::from(2),
         result: Some(serde_json::json!({
             "changes": {
-                Url::from_file_path(foo.clone()).unwrap().to_string(): [
+                Url::from_file_path(&foo).unwrap().to_string(): [
                     {
                         "newText":"Baz",
                         "range":{"start":{"line":6,"character":16},"end":{"line":6,"character":19}}
@@ -100,7 +110,7 @@ fn test_rename() {
                         "range":{"start":{"line":9,"character":4},"end":{"line":9,"character":7}}
                     },
                 ],
-                Url::from_file_path(various_imports.clone()).unwrap().to_string(): [
+                Url::from_file_path(&various_imports).unwrap().to_string(): [
                     {
                         "newText":"Baz",
                         "range":{"start":{"line":5,"character":16},"end":{"line":5,"character":19}}
@@ -110,7 +120,7 @@ fn test_rename() {
                         "range":{"start":{"line":5,"character":26},"end":{"line":5,"character":29}}
                     },
                 ],
-                Url::from_file_path(with_synthetic_bindings.clone()).unwrap().to_string(): [
+                Url::from_file_path(&with_synthetic_bindings).unwrap().to_string(): [
                     {
                         "newText":"Baz",
                         "range":{"start":{"line":5,"character":16},"end":{"character":19,"line":5}}
@@ -120,7 +130,7 @@ fn test_rename() {
                         "range":{"start":{"line":10,"character":4},"end":{"character":7,"line":10}}
                     },
                 ],
-                Url::from_file_path(bar.clone()).unwrap().to_string(): [
+                Url::from_file_path(&bar).unwrap().to_string(): [
                     {
                         "newText":"Baz",
                         "range":{"start":{"line":6,"character":6},"end":{"character":9,"line":6}}
@@ -133,13 +143,7 @@ fn test_rename() {
             }
         })),
         error: None,
-    }));
-
-    run_test_lsp(TestCase {
-        messages_from_language_client: test_messages,
-        expected_messages_from_language_server: expected_responses,
-        indexing_mode: IndexingMode::LazyBlocking,
-        workspace_folders: Some(vec![("test".to_owned(), scope_uri)]),
-        ..Default::default()
     });
+
+    interaction.shutdown();
 }
