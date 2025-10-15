cde9fc8fdfe1ab94b56f62ec2bad68938ad6b166
Rebecca Chen <rechen@meta.com>
2025-10-15 15:46:26 -0700

    Add test cases for issue #1252
    Summary: Adds tests outlining the expected behavior for https://github.com/facebook/pyrefly/issues/1252. The issue as reported isn't quite right, but even if I fix the code samples, pyrefly still does the wrong thing.
    
    Reviewed By: fangyi-zhou
    
    Differential Revision: D84736002
    
    fbshipit-source-id: e80bb6ddc683c6f3945666817bf6beb7c5a8cac4

--- pyrefly/lib/test/generic_restrictions.rs
+++ pyrefly/lib/test/generic_restrictions.rs
@@ -501,3 +501,59 @@ assert_type(g("foo"), Literal["foo"])
 assert_type(g("bar"), Literal["bar"])
     "#,
 );
+
+testcase!(
+    bug = "This should succeed with no errors",
+    test_add_with_constraints,
+    r#"
+def add[T: (int, str)](x: T, y: T) -> T:
+    return x + y # E: `+` is not supported # E: `+` is not supported # E: `int | Unknown` is not assignable to declared return type `T`
+    "#,
+);
+
+testcase!(
+    bug = "Unexpected error in add3",
+    test_add_with_upper_bound,
+    r#"
+# This is not allowed because it's legal to pass something like `add1(0, "1")` to this function.
+def add1[T: int | str](x: T, y: T):
+    return x + y # E: `+` is not supported # E: `+` is not supported
+
+# This is ok.
+def add2[T: int](x: T, y: T) -> int:
+    return x + y
+
+# This is also ok.
+def add3[T: int | float](x: T, y: T) -> int | float:
+    return x + y # E: `+` is not supported
+    "#,
+);
+
+testcase!(
+    bug = "Spurious '`+` is not supported' error",
+    test_add_with_upper_bound_and_bad_return_type,
+    r#"
+# mypy and pyright both reject both of these, so we do, too.
+def add1[T: int](x: T, y: T) -> T:
+    return x + y # E: Returned type `int` is not assignable to declared return type `T`
+def add2[T: int | float](x: T, y: T) -> T:
+    return x + y # E: `+` is not supported # E: Returned type `float | int` is not assignable to declared return type `T`
+    "#,
+);
+
+testcase!(
+    bug = "This should succeed with no errors. Pyrefly pins the types too early due to https://github.com/facebook/pyrefly/issues/105",
+    test_multiple_args_upper_bound,
+    r#"
+from typing import assert_type
+
+def f[T: int | str](x: T, y: T): ...
+f(0, "1") # E: `Literal['1']` is not assignable to parameter `y` with type `int`
+
+class A[T]:
+    def __init__(self, x: T, y: T): ...
+# Note: pyright says the type is A[int | str]; mypy says A[object].
+# Either is okay, but A[int] is definitely wrong and we shouldn't emit the not assignable error.
+assert_type(A(0, "1"), A[int | str]) # E: assert_type(A[int], A[int | str]) # E: `Literal['1']` is not assignable to parameter `y` with type `int`
+    "#,
+);
