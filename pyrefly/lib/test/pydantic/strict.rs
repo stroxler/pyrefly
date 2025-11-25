c10f0f51bf652426aec05dd06cf2b1402889a2cf
Zeina Migeed <migeedz@meta.com>
2025-11-24 17:12:32 -0800

    Add testcase showing that we do not reveal hover enough info for models
    Summary:
    I will followup with some basic support for this by encoding some of the most common types.
    
    In case I face any challanges encoding some of the types (I haven't seen any material challanges yet), I am strongly considering that for a field type annotation 'T' fallback be `T || Any` rather than just `Any`. That way, the hover info is still useful and if we face any tricky situations where we can't encode every single type in the conversion table, the hover information would still be useful.
    
    Reviewed By: rchen152
    
    Differential Revision: D87829610
    
    fbshipit-source-id: a6f9689f4db97f06e30efdaf28da3c51c049bc0d

--- pyrefly/lib/test/pydantic/strict.rs
+++ pyrefly/lib/test/pydantic/strict.rs
@@ -102,3 +102,24 @@ Model2(x=0)
 Model2(x='0')  # E: `Literal['0']` is not assignable to parameter `x`
     "#,
 );
+
+pydantic_testcase!(
+    bug = "Revealed type should be more informative",
+    test_lax_mode_coercion,
+    r#"
+from pydantic import BaseModel
+from typing import reveal_type
+
+class Model(BaseModel):
+    x: int = 0
+
+reveal_type(Model.__init__)  # E: revealed type: (self: Model, *, x: Any = ..., **Unknown) -> None
+
+# int field accepts: int, bool, float, str, bytes  
+Model(x=1)
+Model(x=True)
+Model(x=1.0)
+Model(x='123')
+Model(x=b'123')
+    "#,
+);
