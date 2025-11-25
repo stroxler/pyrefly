a1fa2e3def4d9f3f2e49411703020fcaa9e1c9b3
Rebecca Chen <rechen@meta.com>
2025-11-24 22:07:32 -0800

    Add a couple missing cases to transform_toplevel_func_metadata
    Summary: I noticed that check_toplevel_func_metadata covered some cases that this function didn't.
    
    Reviewed By: yangdanny97
    
    Differential Revision: D87843985
    
    fbshipit-source-id: f4f4cb4d10ac9f86525b347a683a7621f3de59dd

--- crates/pyrefly_types/src/types.rs
+++ crates/pyrefly_types/src/types.rs
@@ -1154,10 +1154,19 @@ impl Type {
                 body: Forallable::Function(func),
             })
             | Type::BoundMethod(box BoundMethod {
-                func: BoundMethodType::Function(func),
+                func:
+                    BoundMethodType::Function(func)
+                    | BoundMethodType::Forall(Forall {
+                        tparams: _,
+                        body: func,
+                    }),
                 ..
             }) => f(&mut func.metadata),
-            Type::Overload(overload) => f(&mut overload.metadata),
+            Type::Overload(overload)
+            | Type::BoundMethod(box BoundMethod {
+                func: BoundMethodType::Overload(overload),
+                ..
+            }) => f(&mut overload.metadata),
             _ => {}
         }
     }
