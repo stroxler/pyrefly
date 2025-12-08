bb20c9f4cba717abc34b7267e954b4b9a87c21b1
Zeina Migeed <migeedz@meta.com>
2025-12-08 13:49:32 -0800

    Add failing testcase for missing synthesized method
    Summary: For issue: https://github.com/facebook/pyrefly/issues/1788
    
    Reviewed By: grievejia
    
    Differential Revision: D88665441
    
    fbshipit-source-id: ec8677301162dfdc2f51d4e454bfa53ec8b62753

--- pyrefly/lib/test/django/fields.rs
+++ pyrefly/lib/test/django/fields.rs
@@ -23,3 +23,27 @@ def test():
     c.name = None
 "#,
 );
+
+django_testcase!(
+    bug = "Synthesize a display field when choices are present",
+    test_get_foo_display,
+    r#"
+from typing import assert_type
+
+from django.db import models
+
+class Person(models.Model):
+    SHIRT_SIZES = {
+        "S": "Small",
+        "M": "Medium",
+        "L": "Large",
+    }
+    name = models.CharField(max_length=60)
+    shirt_size = models.CharField(max_length=2, choices=SHIRT_SIZES)
+
+p = Person(name="Fred Flintstone", shirt_size="L")
+p.save()
+assert_type(p.shirt_size, str) 
+assert_type(p.get_shirt_size_display(), str) # E: Object of class `Person` has no attribute `get_shirt_size_display` # E: assert_type(Any, str)
+"#,
+);
